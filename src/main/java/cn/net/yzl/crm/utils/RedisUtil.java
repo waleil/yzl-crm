f3d46c83ef9de4d303720d05de210a637eba3f9c
王潇 <wangxiao@yuzhilin.net.cn>
2020-12-10 09:20:19 +0800

    修改redis工具类

--- src/main/java/cn/net/yzl/crm/utils/RedisUtil.java
+++ src/main/java/cn/net/yzl/crm/utils/RedisUtil.java
@@ -11,7 +11,6 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author wanghuasheng
  * 基于spring和redis的redisTemplate工具类
  * 针对所有的hash 都是以h开头的方法
  * 针对所有的Set 都是以s开头的方法                    不含通用方法
