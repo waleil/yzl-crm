4c5a601b843e3688c4b80116f49b7f1df4d623b2
jingweitao <jingweitao@yuzhilin.net.cn>
2020-12-11 13:55:08 +0800

    1

--- src/test/java/cn/net/yzl/crm/YzlCrmApplicationTests.java
+++ src/test/java/cn/net/yzl/crm/YzlCrmApplicationTests.java
@@ -1,49 +1,49 @@
-//package cn.net.yzl.crm;
-//
-//import cn.net.yzl.crm.dao.mogo.CityTMogoDAO;
-//import cn.net.yzl.crm.model.mogo.CityDemo;
-//import cn.net.yzl.crm.service.mogo.CityTMogoService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class YzlCrmApplicationTests {
-//
-//    @Autowired
-//    private CityTMogoService service;
-//
-//
-//   // @Test
-//    void contextLoads() {
-//        //service.testTrani();
-//        CityDemo c = new CityDemo();
-//       // c.setName("name2");
-//        service.saveObj2(c);
+package cn.net.yzl.crm;
+
+
+import cn.net.yzl.crm.model.mogo.DemoMongo;
+import cn.net.yzl.crm.service.mogo.DemoMogoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzlCrmApplicationTests {
+
+    @Autowired
+    private DemoMogoService service;
+
+
+   // @Test
+    void contextLoads() {
+        //service.testTrani();
+        DemoMongo c = new DemoMongo();
+        c.setName("name2");
+        service.saveObj2(c);
+    }
+    //@Test
+    void mongoUpdateById() {
+        DemoMongo c = new DemoMongo();
+      //  c.setName("name33");
+    }
+
+    @Test
+    void mongoQueryById() {
+        DemoMongo cityDemo = service.queryById("5fd1c21bbc6ce7149050094f");
+        System.out.println(">>>>>>>>>>>>>>>>>>"+cityDemo);
+    }
+
+   // @Test
+    void mongoSAVE() {
+        DemoMongo c = new DemoMongo();
+     //   c.setName("1111");
+        service.save(c);
+    }
+
+
+
+//    public  void testPage(CityDemo c,int current,int size){
+//        service.page(c,current,size);
 //    }
-//    //@Test
-//    void mongoUpdateById() {
-//        CityDemo c = new CityDemo();
-//      //  c.setName("name33");
-//    }
-//
-//    @Test
-//    void mongoQueryById() {
-//        CityDemo cityDemo = service.queryById("5fd1c21bbc6ce7149050094f");
-//        System.out.println(">>>>>>>>>>>>>>>>>>"+cityDemo);
-//    }
-//
-//   // @Test
-//    void mongoSAVE() {
-//        CityDemo c = new CityDemo();
-//     //   c.setName("1111");
-//        service.save(c);
-//    }
-//
-//
-//
-////    public  void testPage(CityDemo c,int current,int size){
-////        service.page(c,current,size);
-////    }
-//
-//}
+
+}
