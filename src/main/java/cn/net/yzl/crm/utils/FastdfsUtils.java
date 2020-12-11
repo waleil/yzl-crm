6360410064faa0dcd9ac64dd1aabf6cde63f0012
王潇 <wangxiao@yuzhilin.net.cn>
2020-12-11 15:36:33 +0800

    增加fastdfs工具类

---
+++ src/main/java/cn/net/yzl/crm/utils/FastdfsUtils.java
@@ -0,0 +1,86 @@
+package cn.net.yzl.crm.utils;
+
+import com.github.tobato.fastdfs.domain.fdfs.MetaData;
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.domain.proto.storage.DownloadByteArray;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class FastdfsUtils {
+    public static final String DEFAULT_CHARSET = "UTF-8";
+
+    @Autowired
+    private FastFileStorageClient fastFileStorageClient;
+
+    /**
+     * 上传
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public StorePath upload(MultipartFile file) throws IOException {
+        // 设置文件信息
+        Set<MetaData> mataData = new HashSet<>();
+        mataData.add(new MetaData("author", "fastdfs"));
+        mataData.add(new MetaData("description",file.getOriginalFilename()));
+        // 上传
+        StorePath storePath = fastFileStorageClient.uploadFile(
+                file.getInputStream(), file.getSize(),
+                FilenameUtils.getExtension(file.getOriginalFilename()),
+                null);
+        return storePath;
+    }
+
+    /**
+     * 删除
+     * @param path
+     */
+    public void delete(String path) {
+        fastFileStorageClient.deleteFile(path);
+    }
+
+    /**
+     * 删除
+     * @param group
+     * @param path
+     */
+    public void delete(String group,String path) {
+        fastFileStorageClient.deleteFile(group,path);
+    }
+
+    /**
+     * 文件下载
+     * @param path 文件路径，例如：/group1/path=M00/00/00/itstyle.png
+     * @param filename 下载的文件命名
+     * @return
+     */
+    public InputStream download(String path, String filename) throws IOException {
+        // 获取文件
+        StorePath storePath = StorePath.parseFromUrl(path);
+        if (StringUtils.isBlank(filename)) {
+            filename = FilenameUtils.getName(storePath.getPath());
+        }
+        InputStream ins = null;
+        try {
+            byte[] bytes = fastFileStorageClient.downloadFile(storePath.getGroup(), storePath.getPath(), new DownloadByteArray());
+            ins = new ByteArrayInputStream(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ins;
+    }
+}
