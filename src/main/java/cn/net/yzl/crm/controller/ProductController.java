f00ad454bea8cd6e21572c761df84e1cea380345
王潇 <wangxiao@yuzhilin.net.cn>
2020-12-26 18:56:50 +0800

    增加拦截器配置、商品分类和商品属性接口

--- src/main/java/cn/net/yzl/crm/controller/ProductController.java
+++ src/main/java/cn/net/yzl/crm/controller/ProductController.java
@@ -6,11 +6,15 @@ import cn.net.yzl.crm.model.AttributeBean;
 import cn.net.yzl.crm.model.CategoryBean;
 import cn.net.yzl.crm.model.CategoryTO;
 import cn.net.yzl.crm.model.CategoryTreeNode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "商品controller",tags = {"商品访问接口"})
 @RestController
 @RequestMapping("product")
 public class ProductController implements ProductMapperFeign {
@@ -23,6 +27,8 @@ public class ProductController implements ProductMapperFeign {
      * @param attributeBean
      * @return
      */
+    @ApiOperation(value = "添加商品属性")
+    @ApiImplicitParam(name = "attributeBean",value = "商品属性实体类",required = true,dataType = "AttributeBean")
     @PostMapping("insertAttribute")
     public ComResponse insertProductAttribute(@RequestBody AttributeBean attributeBean) {
         return productMapperFeign.insertProductAttribute(attributeBean);
@@ -44,6 +50,8 @@ public class ProductController implements ProductMapperFeign {
      * @param id
      * @return
      */
+    @ApiOperation(value = "查询商品属性")
+    @ApiImplicitParam(name = "id",value = "商品属性id",required = true,dataType = "Integer")
     @GetMapping("selectById")
     public ComResponse selectById(@RequestParam Integer id) {
         return productMapperFeign.selectById(id);
