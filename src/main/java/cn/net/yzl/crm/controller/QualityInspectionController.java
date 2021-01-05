23da3faf078887aace060d2bcbe26a970daf0c7a
liufaguan <liufaguan>
2021-01-05 21:03:36 +0800

    质检原子开发

--- src/main/java/cn/net/yzl/crm/controller/QualityInspectionController.java
+++ src/main/java/cn/net/yzl/crm/controller/QualityInspectionController.java
@@ -4,8 +4,11 @@ import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.crm.model.ProductMarketingQuality;
 import cn.net.yzl.crm.model.StaffTalkQuality;
 import cn.net.yzl.crm.service.micservice.QualityInspectionApi;
-import cn.net.yzl.crm.vo.ProductMarketingQualityVo;
-import cn.net.yzl.crm.vo.StaffTalkQualityVo;
+
+import cn.net.yzl.inspection.model.vo.ProductMarketingQualityVo;
+import cn.net.yzl.inspection.model.vo.StaffTalkQualityVo;
+import cn.net.yzl.inspection.model.WordQuality;
+import cn.net.yzl.inspection.model.vo.WordQualityVo;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,11 +23,108 @@ import org.springframework.web.bind.annotation.*;
  */
 @Api(tags = "质检中心")
 @RestController
-@RequestMapping("/qualityInspection")
+@RequestMapping("inspectionCenter")
 public class QualityInspectionController {
 
     @Autowired
     private QualityInspectionApi qualityInspectionApi;
+
+    /**
+     * author: liufaguan
+     * description: 员工话术质检导入
+     * create time: 2020/12/25 15:54
+     * @Param: null
+     * @return
+     */
+    @ApiOperation(value="员工话术质检导入",httpMethod = "POST")
+    @PostMapping("/importStaffTalkQuality")
+    public ComResponse<Integer> importStaffTalkQuality(@RequestParam("name") String name){
+        return qualityInspectionApi.importStaffTalkQuality();
+    }
+
+
+    /**
+     * author: liufaguan
+     * description: 新增员工话术质检
+     * create time: 2020/12/28 10:41
+     * @Param: staffTalkQuality
+     * @return
+     */
+    @ApiOperation(value="新增员工话术质检",httpMethod = "POST")
+    @PostMapping("/saveStaffTalkQuality")
+    public ComResponse<Integer> saveStaffTalkQuality(@RequestBody StaffTalkQualityVo staffTalkQualityVo){
+        return qualityInspectionApi.saveStaffTalkQuality(staffTalkQualityVo);
+    }
+
+
+    /**
+     * author: liufaguan
+     * description: 员工话术质检列表
+     * create time: 2020/12/28 17:03
+     * @Param: staffTalkQuality
+     * @Param: pageParam
+     * @return
+     */
+    @ApiOperation(value="员工话术质检列表",httpMethod = "GET")
+    @PostMapping("/queryStaffTalkQualityList")
+    public ComResponse<Page<StaffTalkQuality>> queryStaffTalkQualityList(@RequestBody StaffTalkQualityVo staffTalkQualityVo){
+        return qualityInspectionApi.queryStaffTalkQualityList(staffTalkQualityVo);
+    }
+
+    /**
+     * author: liufaguan
+     * description: 查看员工话术质检
+     * create time: 2020/12/28 17:03
+     * @Param: staffTalkQuality
+     * @Param: pageParam
+     * @return
+     */
+    @ApiOperation(value="查看员工话术质检",httpMethod = "GET")
+    @GetMapping("/queryStaffTalkQualityByCode")
+    public ComResponse<StaffTalkQuality> queryStaffTalkQualityByCode(@RequestParam("staffTalkCode") String staffTalkCode){
+        return qualityInspectionApi.queryStaffTalkQualityByCode(staffTalkCode);
+    }
+
+
+    /**
+     * author: liufaguan
+     * description: 更新员工话术质检
+     * create time: 2020/12/28 21:54
+     * @Param: null
+     * @return
+     */
+    @ApiOperation(value="更新员工话术质检",httpMethod = "POST")
+    @PostMapping("/updateStaffTalkQualityByCode")
+    public ComResponse<Integer> updateStaffTalkQualityByCode(@RequestBody StaffTalkQualityVo staffTalkQualityVo){
+        return qualityInspectionApi.updateStaffTalkQualityByCode(staffTalkQualityVo);
+    }
+
+    /**
+     * author: liufaguan
+     * description: 使用某个员工话术质检
+     * create time: 2020/12/28 17:03
+     * @Param: staffTalkQuality
+     * @Param: pageParam
+     * @return
+     */
+    @ApiOperation(value="使用某个员工话术质检",httpMethod = "POST")
+    @PostMapping("/updateStaffTalkQualityUsing")
+    public ComResponse<Boolean> updateStaffTalkQualityUsing(@RequestParam("staffTalkCode") String staffTalkCode){
+        return qualityInspectionApi.updateStaffTalkQualityUsing(staffTalkCode);
+    }
+
+    /**
+     * author: liufaguan
+     * description: 停用某个员工话术
+     * create time: 2020/12/29 15:41
+     * @Param: null
+     * @return
+     */
+    @ApiOperation(value="停用某个员工话术质检",httpMethod = "POST")
+    @PostMapping("/updateStaffTalkQualityDisabled")
+    public ComResponse<Boolean> updateStaffTalkQualityDisabled(@RequestParam("staffTalkCode") String staffTalkCode){
+        return qualityInspectionApi.updateStaffTalkQualityDisabled(staffTalkCode);
+    }
 
     /**
      * author: liufaguan
@@ -35,7 +135,7 @@ public class QualityInspectionController {
      */
     @ApiOperation(value="产品营销话术质检导入",httpMethod = "POST")
     @PostMapping("/importProductMarketingQuality")
-    ComResponse<Integer> importProductMarketingQuality(){
+    ComResponse<Integer> importProductMarketingQuality(@RequestParam("name") String name){
         return qualityInspectionApi.importProductMarketingQuality();
     }
 
@@ -61,9 +161,9 @@ public class QualityInspectionController {
      * @return
      */
     @ApiOperation(value="产品营销话术质检列表",httpMethod = "GET")
-    @GetMapping("/queryProductMarketingQualityList")
-    ComResponse<Page<ProductMarketingQuality>> queryProductMarketingQualityList(@RequestParam Integer current, @RequestParam Integer size){
-       return qualityInspectionApi.queryProductMarketingQualityList( current, size);
+    @PostMapping("/queryProductMarketingQualityList")
+    ComResponse<Page<ProductMarketingQuality>> queryProductMarketingQualityList(@RequestBody ProductMarketingQualityVo productMarketingQualityVo){
+       return qualityInspectionApi.queryProductMarketingQualityList(productMarketingQualityVo);
     }
 
 
@@ -110,98 +210,86 @@ public class QualityInspectionController {
 
     /**
      * author: liufaguan
-     * description: 员工话术质检导入
+     * description: 违禁词质检导入
      * create time: 2020/12/25 15:54
      * @Param: null
      * @return
      */
-    @ApiOperation(value="员工话术质检导入",httpMethod = "POST")
-    @PostMapping("/importStaffTalkQuality")
-    public ComResponse<Integer> importStaffTalkQuality(){
-        return qualityInspectionApi.importStaffTalkQuality();
+    @ApiOperation(value="违禁词质检导入",httpMethod = "POST")
+    @PostMapping("/importWordQuality")
+    ComResponse<Integer> importWordQuality(@RequestParam("name") String name){
+        return qualityInspectionApi.importWordQuality(name);
     }
 
 
     /**
      * author: liufaguan
-     * description: 新增员工话术质检
+     * description: 新增违禁词质检
      * create time: 2020/12/28 10:41
      * @Param: staffTalkQuality
      * @return
      */
-    @ApiOperation(value="新增员工话术质检",httpMethod = "POST")
-    @PostMapping("/saveStaffTalkQuality")
-    public ComResponse<Integer> saveStaffTalkQuality(@RequestBody StaffTalkQualityVo staffTalkQualityVo){
-        return qualityInspectionApi.saveStaffTalkQuality(staffTalkQualityVo);
+    @ApiOperation(value="新增违禁词质检",httpMethod = "POST")
+    @PostMapping("/saveWordQuality")
+    ComResponse<Boolean> saveWordQuality(@RequestBody WordQualityVo wordQualityVo){
+        return qualityInspectionApi.saveWordQuality(wordQualityVo);
     }
 
 
     /**
      * author: liufaguan
-     * description: 员工话术质检列表
+     * description: 违禁词质检列表
      * create time: 2020/12/28 17:03
      * @Param: staffTalkQuality
      * @Param: pageParam
      * @return
      */
-    @ApiOperation(value="员工话术质检列表",httpMethod = "GET")
-    @GetMapping("/queryStaffTalkQualityList")
-    public ComResponse<Page<StaffTalkQuality>> queryStaffTalkQualityList(@RequestParam Integer current, @RequestParam Integer size){
-        return qualityInspectionApi.queryStaffTalkQualityList(current,size);
-    }
-
-    /**
-     * author: liufaguan
-     * description: 查看员工话术质检
-     * create time: 2020/12/28 17:03
-     * @Param: staffTalkQuality
-     * @Param: pageParam
-     * @return
-     */
-    @ApiOperation(value="查看员工话术质检",httpMethod = "GET")
-    @GetMapping("/queryStaffTalkQualityByCode")
-    public ComResponse<StaffTalkQuality> queryStaffTalkQualityByCode(@RequestParam String staffTalkCode){
-        return qualityInspectionApi.queryStaffTalkQualityByCode(staffTalkCode);
+    @ApiOperation(value="违禁词质检列表",httpMethod = "GET")
+    @PostMapping("/queryWordQualityList")
+    ComResponse<Page<WordQuality>> queryWordQualityList(@RequestBody WordQualityVo wordQualityVo){
+        return qualityInspectionApi.queryWordQualityList(wordQualityVo);
     }
 
 
     /**
      * author: liufaguan
-     * description: 更新员工话术质检
+     * description: 更新违禁词质检
      * create time: 2020/12/28 21:54
      * @Param: null
      * @return
      */
-    @ApiOperation(value="更新员工话术质检",httpMethod = "POST")
-    @PostMapping("/updateStaffTalkQualityByCode")
-    public ComResponse<Integer> updateStaffTalkQualityByCode(@RequestBody StaffTalkQualityVo staffTalkQualityVo){
-        return qualityInspectionApi.updateStaffTalkQualityByCode(staffTalkQualityVo);
+    @ApiOperation(value="更新违禁词质检",httpMethod = "POST")
+    @PostMapping("/updateWordQualityByCode")
+    ComResponse<Integer> updateWordQualityByCode(@RequestBody WordQualityVo wordQualityVo){
+        return qualityInspectionApi.updateWordQualityByCode(wordQualityVo);
     }
 
     /**
      * author: liufaguan
-     * description: 使用某个员工话术质检
+     * description: 使用某个违禁词质检
      * create time: 2020/12/28 17:03
      * @Param: staffTalkQuality
      * @Param: pageParam
      * @return
      */
-    @ApiOperation(value="使用某个员工话术质检",httpMethod = "POST")
-    @PostMapping("/updateStaffTalkQualityUsing")
-    public ComResponse<Boolean> updateStaffTalkQualityUsing(@RequestParam String staffTalkCode){
-        return qualityInspectionApi.updateStaffTalkQualityUsing(staffTalkCode);
+    @ApiOperation(value="使用某个违禁词质检",httpMethod = "POST")
+    @PostMapping("/updateWordQualityUsing")
+    ComResponse<Boolean> updateWordQualityUsing(@RequestParam("wordCode") String wordCode){
+        return qualityInspectionApi.updateWordQualityUsing(wordCode);
     }
 
     /**
      * author: liufaguan
-     * description: 停用某个员工话术
+     * description: 停用违禁词质检
      * create time: 2020/12/29 15:41
      * @Param: null
      * @return
      */
-    @ApiOperation(value="停用某个员工话术质检",httpMethod = "POST")
-    @PostMapping("/updateStaffTalkQualityDisabled")
-    public ComResponse<Boolean> updateStaffTalkQualityDisabled(@RequestParam String staffTalkCode){
-        return qualityInspectionApi.updateStaffTalkQualityDisabled(staffTalkCode);
+    @ApiOperation(value="停用违禁词质检",httpMethod = "POST")
+    @PostMapping("/updateWordQualityDisabled")
+    ComResponse<Boolean> updateWordQualityDisabled(@RequestParam("wordCode") String wordCode){
+        return qualityInspectionApi.updateWordQualityDisabled(wordCode);
     }
+
+
 }
--- src/main/java/cn/net/yzl/crm/service/micservice/QualityInspectionApi.java
+++ src/main/java/cn/net/yzl/crm/service/micservice/QualityInspectionApi.java
@@ -3,11 +3,12 @@ package cn.net.yzl.crm.service.micservice;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.crm.model.ProductMarketingQuality;
 import cn.net.yzl.crm.model.StaffTalkQuality;
-import cn.net.yzl.crm.vo.ProductMarketingQualityVo;
-import cn.net.yzl.crm.vo.StaffTalkQualityVo;
+import cn.net.yzl.inspection.model.vo.ProductMarketingQualityVo;
+import cn.net.yzl.inspection.model.vo.StaffTalkQualityVo;
+import cn.net.yzl.inspection.model.WordQuality;
+import cn.net.yzl.inspection.model.vo.WordQualityVo;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,9 +21,85 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @ Description：质检中心api
  * @Version: 1
  */
-@Service
-@FeignClient(value = "qualityInspectionDB",url = "${api.gateway.url}/qualityInspection")
+@FeignClient(value = "inspectionCenter",url = "${api.gateway.url}/qualityInspection")
 public interface QualityInspectionApi {
+
+    /**
+     * author: liufaguan
+     * description: 员工话术质检导入
+     * create time: 2020/12/25 15:54
+     * @Param: null
+     * @return
+     */
+    @PostMapping("/importStaffTalkQuality")
+    ComResponse<Integer> importStaffTalkQuality();
+
+
+    /**
+     * author: liufaguan
+     * description: 新增员工话术质检
+     * create time: 2020/12/28 10:41
+     * @Param: staffTalkQuality
+     * @return
+     */
+    @PostMapping("/saveStaffTalkQuality")
+    ComResponse<Integer> saveStaffTalkQuality(@RequestBody StaffTalkQualityVo staffTalkQualityVo);
+
+
+    /**
+     * author: liufaguan
+     * description: 员工话术质检列表
+     * create time: 2020/12/28 17:03
+     * @Param: staffTalkQuality
+     * @Param: pageParam
+     * @return
+     */
+    @PostMapping("/queryStaffTalkQualityList")
+    ComResponse<Page<StaffTalkQuality>> queryStaffTalkQualityList(@RequestBody StaffTalkQualityVo staffTalkQualityVo);
+
+    /**
+     * author: liufaguan
+     * description: 查看员工话术质检
+     * create time: 2020/12/28 17:03
+     * @Param: staffTalkQuality
+     * @Param: pageParam
+     * @return
+     */
+    @GetMapping("/queryStaffTalkQualityByCode")
+    ComResponse<StaffTalkQuality> queryStaffTalkQualityByCode(@RequestParam("staffTalkCode") String staffTalkCode);
+
+
+    /**
+     * author: liufaguan
+     * description: 更新员工话术质检
+     * create time: 2020/12/28 21:54
+     * @Param: null
+     * @return
+     */
+
+    @PostMapping("/updateStaffTalkQualityByCode")
+    ComResponse<Integer> updateStaffTalkQualityByCode(@RequestBody StaffTalkQualityVo staffTalkQualityVo);
+
+    /**
+     * author: liufaguan
+     * description: 使用某个员工话术质检
+     * create time: 2020/12/28 17:03
+     * @Param: staffTalkQuality
+     * @Param: pageParam
+     * @return
+     */
+    @PostMapping("/updateProductMarketingQualityUsing")
+    ComResponse<Boolean> updateStaffTalkQualityUsing(@RequestParam("staffTalkCode") String staffTalkCode);
+
+    /**
+     * author: liufaguan
+     * description: 停用某个员工话术
+     * create time: 2020/12/29 15:41
+     * @Param: null
+     * @return
+     */
+    @PostMapping("/updateProductMarketingQualityDisabled")
+    ComResponse<Boolean> updateStaffTalkQualityDisabled(@RequestParam("staffTalkCode") String staffTalkCode);
 
     /**
      * author: liufaguan
@@ -52,8 +129,8 @@ public interface QualityInspectionApi {
      * @Param: pageParam
      * @return
      */
-    @GetMapping("queryProductMarketingQualityList")
-    ComResponse<Page<ProductMarketingQuality>>  queryProductMarketingQualityList(@RequestParam Integer current, @RequestParam Integer size);
+    @PostMapping("queryProductMarketingQualityList")
+    ComResponse<Page<ProductMarketingQuality>>  queryProductMarketingQualityList(@RequestBody ProductMarketingQualityVo productMarketingQualityVo);
 
     /**
      * author: liufaguan
@@ -88,78 +165,69 @@ public interface QualityInspectionApi {
 
     /**
      * author: liufaguan
-     * description: 员工话术质检导入
+     * description: 违禁词质检导入
      * create time: 2020/12/25 15:54
      * @Param: null
      * @return
      */
-    @PostMapping("/importStaffTalkQuality")
-    ComResponse<Integer> importStaffTalkQuality();
+    @PostMapping("/importWordQuality")
+    ComResponse<Integer> importWordQuality(@RequestParam("name") String name);
 
 
     /**
      * author: liufaguan
-     * description: 新增员工话术质检
+     * description: 新增违禁词质检
      * create time: 2020/12/28 10:41
      * @Param: staffTalkQuality
      * @return
      */
-    @PostMapping("/saveStaffTalkQuality")
-    ComResponse<Integer> saveStaffTalkQuality(@RequestBody StaffTalkQualityVo staffTalkQualityVo);
+    @PostMapping("/saveWordQuality")
+    ComResponse<Boolean> saveWordQuality(@RequestBody WordQualityVo wordQualityVo);
 
 
     /**
      * author: liufaguan
-     * description: 员工话术质检列表
+     * description: 违禁词质检列表
      * create time: 2020/12/28 17:03
      * @Param: staffTalkQuality
      * @Param: pageParam
      * @return
      */
-    @GetMapping("/queryStaffTalkQualityList")
-    ComResponse<Page<StaffTalkQuality>> queryStaffTalkQualityList(@RequestParam Integer current, @RequestParam Integer size);
-
-    /**
-     * author: liufaguan
-     * description: 查看员工话术质检
-     * create time: 2020/12/28 17:03
-     * @Param: staffTalkQuality
-     * @Param: pageParam
-     * @return
-     */
-    @GetMapping("/queryStaffTalkQualityByCode")
-    ComResponse<StaffTalkQuality> queryStaffTalkQualityByCode(@RequestParam String staffTalkCode);
+    @PostMapping("/queryWordQualityList")
+    ComResponse<Page<WordQuality>> queryWordQualityList(@RequestBody WordQualityVo wordQualityVo);
 
 
     /**
      * author: liufaguan
-     * description: 更新员工话术质检
+     * description: 更新违禁词质检
      * create time: 2020/12/28 21:54
      * @Param: null
      * @return
      */
 
-    @PostMapping("/updateStaffTalkQualityByCode")
-    ComResponse<Integer> updateStaffTalkQualityByCode(@RequestBody StaffTalkQualityVo staffTalkQualityVo);
+    @PostMapping("/updateWordQualityByCode")
+    ComResponse<Integer> updateWordQualityByCode(@RequestBody WordQualityVo wordQualityVo);
 
     /**
      * author: liufaguan
-     * description: 使用某个员工话术质检
+     * description: 使用某个违禁词质检
      * create time: 2020/12/28 17:03
      * @Param: staffTalkQuality
      * @Param: pageParam
      * @return
      */
-    @PostMapping("/updateProductMarketingQualityUsing")
-    ComResponse<Boolean> updateStaffTalkQualityUsing(@RequestParam String staffTalkCode);
+    @PostMapping("/updateWordQualityUsing")
+    ComResponse<Boolean> updateWordQualityUsing(@RequestParam("wordCode") String wordCode);
 
     /**
      * author: liufaguan
-     * description: 停用某个员工话术
+     * description: 停用违禁词质检
      * create time: 2020/12/29 15:41
      * @Param: null
      * @return
      */
-    @PostMapping("/updateProductMarketingQualityDisabled")
-    ComResponse<Boolean> updateStaffTalkQualityDisabled(@RequestParam String staffTalkCode);
+    @PostMapping("/updateStaffTalkQualityDisabled")
+    ComResponse<Boolean> updateWordQualityDisabled(@RequestParam("wordCode") String wordCode);
+
+
 }
