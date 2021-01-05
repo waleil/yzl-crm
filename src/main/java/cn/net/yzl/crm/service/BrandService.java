c84b4926d67404c69b5af5f9920788724275f1d3
lichanghong <lichanghong@yuzhilin.net.cn>
2021-01-05 22:31:39 +0800

    修改包名

--- src/main/java/cn/net/yzl/crm/service/BrandService.java
+++ src/main/java/cn/net/yzl/crm/service/BrandService.java
@@ -3,6 +3,7 @@ package cn.net.yzl.crm.service;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.product.model.vo.brand.BrandVO;
 
+
 public interface BrandService {
     ComResponse getAllBrands(Integer pageNo, Integer pageSize, String keyword);
 
