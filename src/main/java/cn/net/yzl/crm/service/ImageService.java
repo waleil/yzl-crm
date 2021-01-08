e6595c2218ab4abcc16c594afbc36f4f78b83263
Majinbao1997 <2428732826@qq.com>
2021-01-08 16:31:09 +0800

    完成了图片模块的全部内容

--- src/main/java/cn/net/yzl/crm/service/ImageService.java
+++ src/main/java/cn/net/yzl/crm/service/ImageService.java
@@ -1,22 +1,27 @@
 package cn.net.yzl.crm.service;
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.product.model.db.Image;
-import cn.net.yzl.product.model.db.ImageStore;
+import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.product.model.vo.image.ImageDTO;
+import cn.net.yzl.product.model.vo.image.ImageVO;
 import cn.net.yzl.product.model.vo.imageStore.ImageStoreDTO;
+import cn.net.yzl.product.model.vo.imageStore.ImageStoreVO;
 
 import java.util.List;
 
 public interface ImageService {
 
-    ComResponse insert(Image img);
+    ComResponse insert(ImageVO img);
 
-    ComResponse createAlbum(ImageStore is);
+    ComResponse createAlbum(ImageStoreVO is);
 
-    ComResponse<List<ImageDTO>> selectByStoreId(Integer storeId);
+    ComResponse<Page<ImageDTO>> selectByStoreId(Integer storeId, Integer pageNo, Integer pageSize);
 
     ComResponse<Integer> selectTypeById(Integer storeId);
 
     ComResponse<List<ImageStoreDTO>> selectStores(Integer type);
+
+    ComResponse deleteById(Integer id, String userId);
+
+    ComResponse deleteStoreById(Integer id, String userId);
 }
