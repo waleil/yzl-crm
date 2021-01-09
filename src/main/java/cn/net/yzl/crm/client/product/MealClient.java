9091c47a8498ccf43e9bda2b55b5dea229af55d1
Majinbao1997 <2428732826@qq.com>
2021-01-09 14:00:43 +0800

    增加了套餐相关功能

--- src/main/java/cn/net/yzl/crm/client/product/MealClient.java
+++ src/main/java/cn/net/yzl/crm/client/product/MealClient.java
@@ -1,8 +1,16 @@
 package cn.net.yzl.crm.client.product;
 
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.product.model.vo.product.dto.ProductStatusCountDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(name = "mealClient",url = "${api.gateway.url}/productServer/meal/v1")
 public interface MealClient {
+
+    @GetMapping(value = "queryCountByStatus")
+    ComResponse<List<ProductStatusCountDTO>> queryCountByStatus();
 
 }
