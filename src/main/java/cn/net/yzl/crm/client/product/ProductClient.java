1d988cc8a4c0211fd56e0095204c6c89f35be9dc
Majinbao1997 <2428732826@qq.com>
2021-01-11 22:32:39 +0800

    修改了请求方式

--- src/main/java/cn/net/yzl/crm/client/product/ProductClient.java
+++ src/main/java/cn/net/yzl/crm/client/product/ProductClient.java
@@ -5,21 +5,18 @@ import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.product.model.vo.product.dto.*;
 import cn.net.yzl.product.model.vo.product.vo.*;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "productClient", url = "${api.gateway.url}/productServer/product")
+@FeignClient(name = "productClient", url = "localhost:2077/product")
 public interface ProductClient {
 
     @GetMapping(value = "v1/queryCountByStatus")
     ComResponse<List<ProductStatusCountDTO>> queryCountByStatus();
 
-    @GetMapping(value = "v1/queryPageProduct")
-    ComResponse<Page<ProductListDTO>> queryListProduct(@RequestParam("vo") ProductSelectVO vo);
+    @PostMapping(value = "/v1/queryPageProduct")
+    ComResponse<Page<ProductListDTO>> queryListProduct(@RequestBody ProductSelectVO vo);
 
     @PostMapping(value = "v1/edit")
     ComResponse<Void> editProduct(@RequestBody ProductVO vo);
