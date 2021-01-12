31777649ac429447d08e87ca39cede605c2ab7f1
zhangruisong <zhangruisong@yuzhilin.net.cn>
2021-01-12 16:04:22 +0800

    保存收获地址接口

--- src/main/java/cn/net/yzl/crm/service/micservice/MemberFien.java
+++ src/main/java/cn/net/yzl/crm/service/micservice/MemberFien.java
@@ -13,52 +13,52 @@ import java.util.List;
 /**
  * 顾客服务接口
  */
-@FeignClient(name = "crmCustomer",url = "http://api.staff.yuzhilin.net.cn/crmCustomer")
+@FeignClient(name = "crmCustomer",url = "${api.gateway.url}/crmCustomer/member")
 //@FeignClient(value = "yzl-crm-customer-api")
 public interface MemberFien {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/member/v1/getMemberListByPage")
+    @RequestMapping(method = RequestMethod.POST, value = "/v1/getMemberListByPage")
     GeneralResult<Page<Member>> listPage(@RequestBody MemberSerchDTO dto);
 
     @ApiOperation("保存会员信息")
-    @PostMapping("/member/v1/save")
+    @PostMapping("/v1/save")
     GeneralResult<Boolean> save(@RequestBody Member dto);
 
     @ApiOperation("修改会员信息")
-    @PostMapping("/member/v1/updateByMemberCard")
+    @PostMapping("/v1/updateByMemberCard")
     GeneralResult<Boolean> updateByMemberCard(@RequestBody Member dto);
 
     @ApiOperation("根据卡号获取会员信息")
-    @GetMapping("/member/v1/getMember")
+    @GetMapping("/v1/getMember")
     GeneralResult<Member> getMember(@RequestParam("memberCard") String  memberCard);
 
     @ApiOperation("获取会员等级")
-    @GetMapping("/member/v1/getMemberGrad")
+    @GetMapping("/v1/getMemberGrad")
     GeneralResult<List<MemberGrad>> getMemberGrad();
 
     @ApiOperation("获取顾客联系方式信息，包括手机号，座机号")
-    @GetMapping("/member/v1/getMemberPhoneList")
-    GeneralResult<List<MemberPhone>> getMemberPhoneList(String member_card);
+    @GetMapping("/v1/getMemberPhoneList")
+    GeneralResult<List<MemberPhone>> getMemberPhoneList(@RequestParam("member_card") String member_card);
 
     @ApiOperation("根据手机号获取顾客信息（可用来判断手机号是否被注册，如果被注册则返回注册顾客实体）")
-    @GetMapping("/member/v1/getMemberByPhone")
-    GeneralResult<Member> getMemberByPhone(String phone);
+    @GetMapping("/v1/getMemberByPhone")
+    GeneralResult<Member> getMemberByPhone(@RequestParam("phone") String phone);
 
     @ApiOperation("设置顾客为会员")
-    @GetMapping("/member/v1/setMemberToVip")
-    void   setMemberToVip(String member_card);
+    @GetMapping("/v1/setMemberToVip")
+    void   setMemberToVip(@RequestParam("member_card") String member_card);
 
     @ApiOperation("获取顾客购买商品")
-    @GetMapping("/member/v1/getMemberProductEffectList")
-    GeneralResult<List<MemberProductEffect>> getMemberProductEffectList(String member_card);
+    @GetMapping("/v1/getMemberProductEffectList")
+    GeneralResult<List<MemberProductEffect>> getMemberProductEffectList(@RequestParam("member_card") String member_card);
 
     @ApiOperation("获取顾客咨询商品")
-    @GetMapping("/member/v1/getProductConsultationList")
-    GeneralResult<List<ProductConsultation>>  getProductConsultationList(String member_card);
+    @GetMapping("/v1/getProductConsultationList")
+    GeneralResult<List<ProductConsultation>>  getProductConsultationList(@RequestParam("member_card") String member_card);
 
     @ApiOperation("获取顾客病症")
     @GetMapping("/member/v1/getMemberDisease")
-    GeneralResult<List<MemberDisease>> getMemberDisease(String member_card);
+    GeneralResult<List<MemberDisease>> getMemberDisease(@RequestParam("member_card") String member_card);
 
     @ApiOperation("新增收货地址")
     @GetMapping("/member/v1/addReveiverAddress")
@@ -71,11 +71,11 @@ public interface MemberFien {
 
     @ApiOperation("获取收获地址")
     @GetMapping("/member/v1/getReveiverAddress")
-    GeneralResult<List<ReveiverAddress>> getReveiverAddress(String member_card);
+    GeneralResult<List<ReveiverAddress>> getReveiverAddress(@RequestParam("member_card") String member_card);
 
     @ApiOperation("获取购买能力")
     @GetMapping("/member/v1/getMemberOrderStat")
-    GeneralResult<MemberOrderStat> getMemberOrderStat(String member_card);
+    GeneralResult<MemberOrderStat> getMemberOrderStat(@RequestParam("member_card") String member_card);
 
 
     @ApiOperation("新增购买能力")
@@ -96,5 +96,5 @@ public interface MemberFien {
 
     @ApiOperation("获取顾客行为偏好")
     @GetMapping("/member/v1/getMemberAction")
-    GeneralResult<MemberAction> getMemberAction(String member_card);
+    GeneralResult<MemberAction> getMemberAction(@RequestParam("member_card") String member_card);
 }
