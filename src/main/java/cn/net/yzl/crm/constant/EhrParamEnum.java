dc6fb0e622fda68fa2143b9cd4de269b7f269bc8
许微 <xuwei@yuzhilin.net.cn>
2021-01-15 20:06:57 +0800

    新增ehr业务属性code常量

--- src/main/java/cn/net/yzl/crm/constant/EhrParamEnum.java
+++ src/main/java/cn/net/yzl/crm/constant/EhrParamEnum.java
@@ -15,4 +15,13 @@ public enum EhrParamEnum {
      * 职场
      */
     public static final  String EHR_DICT_WORKPLACE_STATUS = "workplace";
+
+    /**
+     * 业务属性   热线
+     */
+    public static final  int EHR_HOTLINE_BUSINESS_ATTRIBUTE_CODE = 42;
+    /**
+     * 业务属性   回访
+     */
+    public static final  int EHR_VISIT_BUSINESS_ATTRIBUTE_CODE = 43;
 }
