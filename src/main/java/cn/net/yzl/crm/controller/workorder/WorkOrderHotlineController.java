679e20ec053569923b5068bb89a6cd77b228146e
李贺彦 <w-liheyan@yuzhilin.net.cn>
2021-01-15 15:20:30 +0800

    1：热线工单-分页列表

--- src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderHotlineController.java
+++ src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderHotlineController.java
@@ -1,29 +1,22 @@
 package cn.net.yzl.crm.controller.workorder;
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.common.enums.ResponseCodeEnums;
-import cn.net.yzl.common.util.DateFormatUtil;
+import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.crm.client.workorder.WorkOrderHotlineClient;
-import cn.net.yzl.crm.client.workorder.WorkOrderRuleClient;
-import cn.net.yzl.crm.dto.workorder.UpdateMoreAdjustDTO;
-import cn.net.yzl.crm.dto.workorder.UpdateRecyclingDTO;
-import cn.net.yzl.crm.dto.workorder.UpdateSingleAdjustDTO;
 import cn.net.yzl.workorder.model.db.WorkOrderHotlineBean;
-import cn.net.yzl.workorder.model.db.WorkOrderHotlineFlowBean;
-import cn.net.yzl.workorder.model.db.WorkOrderRuleBean;
+import cn.net.yzl.workorder.model.dto.FindWorkOrderHotlinePageListDTO;
+import cn.net.yzl.workorder.model.dto.UpdateMoreAdjustDTO;
+import cn.net.yzl.workorder.model.dto.UpdateRecyclingDTO;
+import cn.net.yzl.workorder.model.dto.UpdateSingleAdjustDTO;
+import cn.net.yzl.workorder.model.vo.FindWorkOrderHotlinePageListVO;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import java.util.Date;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -68,6 +61,17 @@ public class WorkOrderHotlineController {
     @ApiOperation(value = "热线工单：多数据调整", notes = "热线工单：多数据调整")
     public ComResponse<Void> updateMoreAdjust(@Validated @RequestBody UpdateMoreAdjustDTO updateMoreAdjustDTO){
         return workOrderHotlineClient.updateMoreAdjust(updateMoreAdjustDTO);
+    }
+
+    /**
+     * 热线工单：查询热线工单列表
+     * @param
+     * @return
+     */
+    @GetMapping("v1/list")
+    @ApiOperation(value = "热线工单：多数据调整", notes = "热线工单：多数据调整")
+    public ComResponse<Page<FindWorkOrderHotlinePageListVO>> findWorkOrderHotlinePageList(@RequestBody FindWorkOrderHotlinePageListDTO findWorkOrderHotlinePageListDTO){
+        return workOrderHotlineClient.findWorkOrderHotlinePageList(findWorkOrderHotlinePageListDTO);
     }
 
 }
