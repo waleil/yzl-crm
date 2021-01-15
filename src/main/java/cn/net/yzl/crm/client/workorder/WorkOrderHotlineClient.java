fc1b7ee4b4d1ec460e83999d4672e15e409983bb
zhangyongyong <zhangyongyong@yuzhilin.net.cn>
2021-01-15 18:53:43 +0800

    冲突

--- src/main/java/cn/net/yzl/crm/client/workorder/WorkOrderHotlineClient.java
+++ src/main/java/cn/net/yzl/crm/client/workorder/WorkOrderHotlineClient.java
@@ -2,20 +2,26 @@ package cn.net.yzl.crm.client.workorder;
 
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.crm.dto.workorder.UpdateMoreAdjustDTO;
-import cn.net.yzl.crm.dto.workorder.UpdateRecyclingDTO;
-import cn.net.yzl.crm.dto.workorder.UpdateSingleAdjustDTO;
-import java.util.List;
+import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.workorder.model.db.WorkOrderHotlineBean;
+import cn.net.yzl.workorder.model.dto.FindWorkOrderHotlinePageListDTO;
+import cn.net.yzl.workorder.model.dto.UpdateMoreAdjustDTO;
+import cn.net.yzl.workorder.model.dto.UpdateRecyclingDTO;
+import cn.net.yzl.workorder.model.dto.UpdateSingleAdjustDTO;
+import cn.net.yzl.workorder.model.vo.FindWorkOrderHotlinePageListVO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *  工单规则配置相关功能
  */
 @Service
 @FeignClient(name = "WorkOrderHotline",url = "${api.gateway.url}/workorderServer/hotline")
+//@FeignClient(name = "WorkOrderHotline",url = "127.0.0.1:4602/hotline")
 public interface WorkOrderHotlineClient {
 
     /**
@@ -23,7 +29,7 @@ public interface WorkOrderHotlineClient {
      * @param updateRecyclingDTO
      * @return
      */
-    @PostMapping("v1/updateRecycling")
+    @RequestMapping(value = "v1/updateRecycling",method = RequestMethod.POST)
     ComResponse<Void> updateRecycling(@RequestBody UpdateRecyclingDTO updateRecyclingDTO);
 
 
@@ -32,7 +38,7 @@ public interface WorkOrderHotlineClient {
      * @param updateSingleAdjustDTO
      * @return
      */
-    @PostMapping("v1/updateSingleAdjust")
+    @RequestMapping(value = "v1/updateSingleAdjust",method = RequestMethod.POST)
     ComResponse<Void> updateSingleAdjust(@RequestBody UpdateSingleAdjustDTO updateSingleAdjustDTO);
 
     /**
@@ -40,6 +46,14 @@ public interface WorkOrderHotlineClient {
      * @param updateMoreAdjustDTO
      * @return
      */
-    @PostMapping("v1/updateMoreAdjust")
-    ComResponse<Void> updateMoreAdjust(UpdateMoreAdjustDTO updateMoreAdjustDTO);
+    @RequestMapping(value = "v1/updateMoreAdjust",method = RequestMethod.POST)
+    ComResponse<Void> updateMoreAdjust(@RequestBody UpdateMoreAdjustDTO updateMoreAdjustDTO);
+
+    /**
+     * 热线工单：查询热线工单列表
+     * @param findWorkOrderHotlinePageListDTO
+     * @return
+     */
+    @RequestMapping(value = "v1/findWorkOrderHotlinePageList",method = RequestMethod.GET)
+    ComResponse<Page<FindWorkOrderHotlinePageListVO>> findWorkOrderHotlinePageList(@SpringQueryMap FindWorkOrderHotlinePageListDTO findWorkOrderHotlinePageListDTO);
 }
