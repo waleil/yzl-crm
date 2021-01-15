c69581dd35df821cc2826a332e2e1457b8e6e07e
许微 <xuwei@yuzhilin.net.cn>
2021-01-15 15:48:04 +0800

    Merge branch 'develop' of /Users/xuwei/yzlworkspace/yzl-crm with conflicts.

--- src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderHotlineController.java
+++ src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderHotlineController.java
@@ -1,19 +1,14 @@
 package cn.net.yzl.crm.controller.workorder;
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.crm.client.workorder.WorkOrderHotlineClient;
-import cn.net.yzl.workorder.model.db.WorkOrderHotlineBean;
-import cn.net.yzl.workorder.model.dto.FindWorkOrderHotlinePageListDTO;
-import cn.net.yzl.workorder.model.dto.UpdateMoreAdjustDTO;
-import cn.net.yzl.workorder.model.dto.UpdateRecyclingDTO;
-import cn.net.yzl.workorder.model.dto.UpdateSingleAdjustDTO;
-import cn.net.yzl.workorder.model.vo.FindWorkOrderHotlinePageListVO;
+import cn.net.yzl.crm.dto.workorder.UpdateMoreAdjustDTO;
+import cn.net.yzl.crm.dto.workorder.UpdateRecyclingDTO;
+import cn.net.yzl.crm.dto.workorder.UpdateSingleAdjustDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,17 +56,6 @@ public class WorkOrderHotlineController {
     @ApiOperation(value = "热线工单：多数据调整", notes = "热线工单：多数据调整")
     public ComResponse<Void> updateMoreAdjust(@Validated @RequestBody UpdateMoreAdjustDTO updateMoreAdjustDTO){
         return workOrderHotlineClient.updateMoreAdjust(updateMoreAdjustDTO);
-    }
-
-    /**
-     * 热线工单：查询热线工单列表
-     * @param
-     * @return
-     */
-    @GetMapping("v1/list")
-    @ApiOperation(value = "热线工单：多数据调整", notes = "热线工单：多数据调整")
-    public ComResponse<Page<FindWorkOrderHotlinePageListVO>> findWorkOrderHotlinePageList(@RequestBody FindWorkOrderHotlinePageListDTO findWorkOrderHotlinePageListDTO){
-        return workOrderHotlineClient.findWorkOrderHotlinePageList(findWorkOrderHotlinePageListDTO);
     }
 
 }
