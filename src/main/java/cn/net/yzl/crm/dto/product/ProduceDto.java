384da44c45ce156e4bb4e7c9685d24fb937d437e
liuchangfu <liuchangfu@yuzhilin.net.cn>
2021-01-18 09:38:27 +0800

    培训过的商品

---
+++ src/main/java/cn/net/yzl/crm/dto/product/ProduceDto.java
@@ -0,0 +1,17 @@
+package cn.net.yzl.crm.dto.product;
+
+import lombok.Data;
+
+/**
+ * @author: liuChangFu
+ * @date: 2021/1/16 23:17
+ * @desc: //TODO  请说明该类的用途
+ **/
+@Data
+public class ProduceDto {
+
+    private String productCode;
+
+    private String name;
+
+}
