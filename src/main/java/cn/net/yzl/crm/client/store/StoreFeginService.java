26b8c168638d5d75510af5fdb1b19d96b5fe133c
王潇 <wangxiao@yuzhilin.net.cn>
2021-01-20 15:36:17 +0800

    修改上传接口

--- src/main/java/cn/net/yzl/crm/client/store/StoreFeginService.java
+++ src/main/java/cn/net/yzl/crm/client/store/StoreFeginService.java
@@ -2,6 +2,7 @@ package cn.net.yzl.crm.client.store;
 
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.model.dto.DepartDto;
 import cn.net.yzl.model.dto.StoreDto;
 import cn.net.yzl.model.dto.StoreLocalDto;
 import cn.net.yzl.model.pojo.StoreLocalPo;
@@ -13,10 +14,8 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -108,6 +107,12 @@ public interface StoreFeginService {
     public ComResponse stockInquiry(@RequestParam("pageNo") Integer pageNo, @RequestParam("pageSize") Integer pageSize,
                                     @RequestParam(value = "codeAndName",required = false) String codeAndName    ,
                                     @RequestParam(value = "storeName",required = false) String storeName);
+
+
+    @ApiOperation(value = "获取已存在的财务归属", notes = "获取已存在的财务归属", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @RequestMapping(value = "store/v1/getAllFinanceDepart", method = RequestMethod.GET)
+    ComResponse<List<DepartDto>> getAllFinanceDepart();
+
 
 
 }
