f6ea1dabce329f91304174703d5cb885f0cf3145
renjudong <YZL_15139769986>
2021-01-20 23:21:06 +0800

    代码微调

--- src/main/java/cn/net/yzl/crm/controller/logistics/LogisticsController.java
+++ src/main/java/cn/net/yzl/crm/controller/logistics/LogisticsController.java
@@ -1,26 +1,14 @@
 package cn.net.yzl.crm.controller.logistics;
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.common.entity.Page;
-import cn.net.yzl.crm.client.OrderSettingClient;
 import cn.net.yzl.crm.service.micservice.LogisticsFien;
-import cn.net.yzl.crm.service.product.ProductService;
 import cn.net.yzl.logistics.model.vo.logistics.ExpressCodeVo;
-import cn.net.yzl.order.model.vo.order.OrderCheckSettingDTO;
-import cn.net.yzl.order.model.vo.order.OrderCheckSettingProduct;
-import cn.net.yzl.order.model.vo.order.UpdateOrderCheckSettingDTO;
-import cn.net.yzl.product.model.vo.product.dto.ProductListDTO;
-import cn.net.yzl.product.model.vo.product.vo.ProductSelectVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
