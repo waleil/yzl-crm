d5e0eeb8c58317077fe4fd6b79d7c0d1c09e5bdc
wangshuaidong <wangshuaidong@yuzhilin.net.cn>
2021-01-21 14:32:27 +0800

    仓储中心采购退货单接口修改

--- src/main/java/cn/net/yzl/crm/client/store/PurchaseReturnFeginService.java
+++ src/main/java/cn/net/yzl/crm/client/store/PurchaseReturnFeginService.java
@@ -1,8 +1,12 @@
 package cn.net.yzl.crm.client.store;
 
 import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.model.dto.PurchaseReturnOrderDto;
+import cn.net.yzl.model.dto.PurchaseReturnResDto;
 import cn.net.yzl.model.dto.PurchaseReturnReviewDto;
+import cn.net.yzl.model.vo.PurReturnOrderAddVo;
+import cn.net.yzl.model.vo.PurReturnOrderUpdateVo;
 import cn.net.yzl.model.vo.PurchaseReturnCondition;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -26,15 +30,15 @@ public interface PurchaseReturnFeginService {
      */
     @ApiOperation(value = "采购退货单分页列表", notes = "采购退货单分页列表")
     @PostMapping("purchaseReturn/v1/page")
-    ComResponse page(@RequestBody PurchaseReturnCondition purchaseReturnCondition);
+    ComResponse<Page<PurchaseReturnResDto>> page(@RequestBody PurchaseReturnCondition purchaseReturnCondition);
 
     @ApiOperation(value = "采购退货单新增")
     @PostMapping("purchaseReturn/v1/add")
-    ComResponse add(@RequestBody PurchaseReturnOrderDto purchaseReturnOrderDto);
+    ComResponse add(@RequestBody PurReturnOrderAddVo purReturnOrderAddVo);
 
     @ApiOperation(value = "采购退货单更新")
     @PostMapping("purchaseReturn/v1/update")
-    ComResponse update(@RequestBody PurchaseReturnOrderDto purchaseReturnOrderDto);
+    ComResponse update(@RequestBody PurReturnOrderUpdateVo purReturnOrderUpdateVo);
 
     @ApiOperation(value = "采购退货单审核")
     @PostMapping("purchaseReturn/v1/review")
@@ -43,7 +47,7 @@ public interface PurchaseReturnFeginService {
     @ApiOperation(value = "采购退货单详情")
     @ApiImplicitParam(name = "id", value = "采购退货单id", required = true, dataType = "Int", paramType = "query")
     @GetMapping("purchaseReturn/v1/detail")
-    ComResponse detail(@RequestParam("id") Integer id);
+    ComResponse<PurchaseReturnResDto> detail(@RequestParam("id") Integer id);
 
 
 
@@ -63,5 +67,5 @@ public interface PurchaseReturnFeginService {
      */
     @ApiOperation(value = "采购退货单审核列表", notes = "采购退货单审核列表")
     @PostMapping("purchaseReturn/v1/review/page")
-    ComResponse reviewPage(@RequestBody PurchaseReturnCondition purchaseReturnCondition);
+    ComResponse<Page<PurchaseReturnResDto>> reviewPage(@RequestBody PurchaseReturnCondition purchaseReturnCondition);
 }
