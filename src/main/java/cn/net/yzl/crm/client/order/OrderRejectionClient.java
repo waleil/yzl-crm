95c251655d7d4f047c84520506e83842145b12f8
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-01-23 16:26:46 +0800

    pageNum to pageNo

--- src/main/java/cn/net/yzl/crm/client/order/OrderRejectionClient.java
+++ src/main/java/cn/net/yzl/crm/client/order/OrderRejectionClient.java
@@ -29,7 +29,7 @@ public interface OrderRejectionClient {
      * @return
      */
     @GetMapping("v1/getOrderRejectionList")
-    ComResponse<Page<OrderRejectionPageDTO>> getOrderRejectionList(@RequestParam(value = "orderNo") String orderNo, @RequestParam(value = "pageNo") Integer pageNo, @RequestParam(value = "pageSize") Integer pageSize);
+    ComResponse<Page<OrderRejectionPageDTO>> getOrderRejectionList(@RequestParam String orderNo, @RequestParam Integer pageNo, @RequestParam Integer pageSize);
 
     /**
      * 新增拒收单
@@ -47,12 +47,12 @@ public interface OrderRejectionClient {
      * @return
      */
     @GetMapping("v1/getCouponListByOrderNo")
-    ComResponse<List<OrderCouponDTO>> getListByOrderNo(@RequestParam(value = "orderNo") String orderNo);
+    ComResponse<List<OrderCouponDTO>> getListByOrderNo(@RequestParam String orderNo);
 
 
     @GetMapping("v1/getOrderList")
     @ApiOperation(value = "查询已发货、待发货订单")
-    ComResponse<Page<OderListResDTO>> selectOrderList(@RequestParam(value = "pageNum") Integer pageNum, @RequestParam(value = "pageSize") Integer pageSize, @RequestParam(value = "orderNo") String orderNo);
+    ComResponse<Page<OderListResDTO>> selectOrderList(@RequestParam Integer pageNo, @RequestParam Integer pageSize, @RequestParam String orderNo);
 
     /**
      * 查询拒收单详情
@@ -62,5 +62,5 @@ public interface OrderRejectionClient {
      */
     @GetMapping("v1/getOrderRejectionDetail")
     @ApiOperation(value = "查询拒收单详情")
-    ComResponse<OrderRejectionDetailDTO> getOrderRejectionDetail(@RequestParam(value = "orderNo") String orderNo);
+    ComResponse<OrderRejectionDetailDTO> getOrderRejectionDetail(@RequestParam String orderNo);
 }
