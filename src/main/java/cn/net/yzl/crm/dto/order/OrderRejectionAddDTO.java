8a5bb3916a4c91f784bce367b26399f8dc14efb3
周长松 <zhouchangsong@yuzhilin.net.cn>
2021-01-23 18:38:53 +0800

    @title:变更仓库号类型 @author:周长松

--- src/main/java/cn/net/yzl/crm/dto/order/OrderRejectionAddDTO.java
+++ src/main/java/cn/net/yzl/crm/dto/order/OrderRejectionAddDTO.java
@@ -25,5 +25,5 @@ public class OrderRejectionAddDTO implements Serializable {
     private Integer rejectType;
 
     @ApiModelProperty("仓库号")
-    private Long storeNo;
+    private String storeNo;
 }
