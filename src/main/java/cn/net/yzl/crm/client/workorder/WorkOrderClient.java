df2c77cb6f866b850c0ffe08e5be82c7b05d8d3b
w-weilongjie <w-weilongjie@yuzhilin.net.cn>
2021-01-25 11:34:06 +0800

    提交工单类应用层修改和接口提交

--- src/main/java/cn/net/yzl/crm/client/workorder/WorkOrderClient.java
+++ src/main/java/cn/net/yzl/crm/client/workorder/WorkOrderClient.java
@@ -1,10 +1,18 @@
 package cn.net.yzl.crm.client.workorder;
 
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.workorder.model.dto.IsListPageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "workOrder",url = "${api.gateway.url}/workorderServer")
-public class WorkOrderClient {
+/**
+ * 智能工单
+ */
+@FeignClient(name = "workOrder",url = "${api.gateway.url}/workorderServer/workOrder")
+//@FeignClient(name = "workOrder",url = "127.0.0.1:4602/workorderServer/workOrder")
+public interface WorkOrderClient {
 
-
+    @PostMapping(value = "v1/isListPage")
+    ComResponse<WorkOrderClient> isListPage(IsListPageDTO isListPageDTO);
 }
--- src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderController.java
+++ src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderController.java
@@ -1,7 +1,12 @@
 package cn.net.yzl.crm.controller.workorder;
 
+import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.crm.client.workorder.WorkOrderClient;
+import cn.net.yzl.workorder.model.db.WorkOrderBean;
+import cn.net.yzl.workorder.model.dto.IsListPageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ public class WorkOrderController {
 
     @Autowired
     private WorkOrderClient workOrderClient;
+
+    @PostMapping(value = "v1/isListPage")
+    public ComResponse<WorkOrderBean> isListPage(@RequestBody IsListPageDTO isListPageDTO){
+        workOrderClient.isListPage(isListPageDTO);
+
+    }
 }
