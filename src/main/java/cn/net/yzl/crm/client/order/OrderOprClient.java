e3ab7c6c9bd4a37def4d5f44f227ef7fcd47759a
程宇 <chengyu@yuzhilin.net.cn>
2021-01-25 21:19:32 +0800

    取消订单

--- src/main/java/cn/net/yzl/crm/client/order/OrderOprClient.java
+++ src/main/java/cn/net/yzl/crm/client/order/OrderOprClient.java
@@ -4,11 +4,12 @@ import cn.net.yzl.common.entity.ComResponse;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "OrderOprClient",url = "${api.gateway.url}/orderService/orderOpr")
 public interface OrderOprClient {
 
     @ApiOperation(value = "取消订单")
     @PostMapping("v1/cancleOrderM")
-    public ComResponse<Boolean> cancleOrderM( String orderNo);
+    public ComResponse<Boolean> cancleOrderM( @RequestParam("orderNo") String orderNo);
 }
