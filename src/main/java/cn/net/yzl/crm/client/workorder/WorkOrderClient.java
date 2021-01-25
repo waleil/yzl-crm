7c7226fac192edd5af1fcf7a7233c848c8ed4dc2
李贺彦 <w-liheyan@yuzhilin.net.cn>
2021-01-25 13:29:23 +0800

    1：智能工单-热线工单列表

--- src/main/java/cn/net/yzl/crm/client/workorder/WorkOrderClient.java
+++ src/main/java/cn/net/yzl/crm/client/workorder/WorkOrderClient.java
@@ -3,7 +3,9 @@ package cn.net.yzl.crm.client.workorder;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.workorder.model.db.WorkOrderBean;
+import cn.net.yzl.workorder.model.dto.FindWorkOrderHotlinePageListDTO;
 import cn.net.yzl.workorder.model.dto.IsListPageDTO;
+import cn.net.yzl.workorder.model.vo.FindWorkOrderHotlinePageListVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +14,17 @@ import org.springframework.web.bind.annotation.PostMapping;
  * 智能工单
  */
 @FeignClient(name = "workOrder",url = "${api.gateway.url}/workorderServer/workOrder")
-//@FeignClient(name = "workOrder",url = "127.0.0.1:4602/workorderServer/workOrder")
+//@FeignClient(name = "workOrder",url = "127.0.0.1:4602/workOrder")
 public interface WorkOrderClient {
 
     @PostMapping(value = "v1/isListPage")
     ComResponse<Page<WorkOrderBean>> isListPage(IsListPageDTO isListPageDTO);
+
+    /**
+     * 智能工单: 热线工单管理-列表
+     * @param findWorkOrderHotlinePageListDTO
+     * @return
+     */
+    @PostMapping(value = "v1/pageList")
+    ComResponse<Page<FindWorkOrderHotlinePageListVO>> pageList(FindWorkOrderHotlinePageListDTO findWorkOrderHotlinePageListDTO);
 }
--- src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderController.java
+++ src/main/java/cn/net/yzl/crm/controller/workorder/WorkOrderController.java
@@ -3,9 +3,12 @@ package cn.net.yzl.crm.controller.workorder;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.crm.client.workorder.WorkOrderClient;
+import cn.net.yzl.workorder.model.dto.FindWorkOrderHotlinePageListDTO;
+import cn.net.yzl.workorder.model.vo.FindWorkOrderHotlinePageListVO;
 import io.swagger.annotations.Api;
 import cn.net.yzl.workorder.model.db.WorkOrderBean;
 import cn.net.yzl.workorder.model.dto.IsListPageDTO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,5 +27,12 @@ public class WorkOrderController {
     public ComResponse<Page<WorkOrderBean>> isListPage(@RequestBody IsListPageDTO isListPageDTO){
         ComResponse<Page<WorkOrderBean>> listPage = workOrderClient.isListPage(isListPageDTO);
         return listPage;
+    }
+
+    @PostMapping("v1/pageList")
+    @ApiOperation(value = "智能工单: 热线工单管理-列表", notes = "智能工单: 热线工单管理-列表")
+    public ComResponse<Page<FindWorkOrderHotlinePageListVO>> pageList(@RequestBody FindWorkOrderHotlinePageListDTO findWorkOrderHotlinePageListDTO) {
+        ComResponse<Page<FindWorkOrderHotlinePageListVO>> pageComResponse = workOrderClient.pageList(findWorkOrderHotlinePageListDTO);
+        return pageComResponse;
     }
 }
