73f0ce827b7d74c757f563fee54385fa78232d38
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-01-26 10:03:15 +0800

    OutStoreWarningClient.java OrderWarningOutStoreController.java

--- src/main/java/cn/net/yzl/crm/client/order/OutStoreWarningClient.java
+++ src/main/java/cn/net/yzl/crm/client/order/OutStoreWarningClient.java
@@ -1,10 +1,13 @@
 package cn.net.yzl.crm.client.order;
 
 import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.order.model.vo.order.OrderWarningOutStorePageDTO;
 import cn.net.yzl.order.model.vo.order.OutStoreWarningDTO;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author zhouchangsong
@@ -12,7 +15,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "outStoreWarningClient", url = "${api.gateway.url}/orderService/outStoreWarning")
 public interface OutStoreWarningClient {
 
-    @GetMapping("v1/getOutStoreWarningDetail")
-    @ApiOperation(value = "查询出库预警订单信息")
-    ComResponse<OutStoreWarningDTO> getOutStoreWarningDetail();
+	@GetMapping("v1/getOutStoreWarningDetail")
+	@ApiOperation(value = "查询出库预警订单信息")
+	ComResponse<OutStoreWarningDTO> getOutStoreWarningDetail();
+
+	@GetMapping("v1/getPageList")
+	ComResponse<Page<OrderWarningOutStorePageDTO>> getPageList(@RequestParam Integer pageNo,
+			@RequestParam Integer pageSize, @RequestParam String orderNo);
 }
