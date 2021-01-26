d6d7c4df3ede9128cab272451a408b35a89d9e7e
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-01-26 16:40:51 +0800

    OrderRestControllerTests.java

--- src/test/java/cn/net/yzl/crm/test/OrderRestControllerTests.java
+++ src/test/java/cn/net/yzl/crm/test/OrderRestControllerTests.java
@@ -87,21 +87,56 @@ public class OrderRestControllerTests {
 	}
 
 	@Test
-	public void testSubmitOrder() {
+	public void testSubmitOrderForProduct() {
 		try {
 			OrderIn order = new OrderIn();
 			OrderDetailIn od1 = new OrderDetailIn();
-			od1.setProductCode("10000107");
+			od1.setProductCode("10000142");
 			od1.setMealFlag(CommonConstant.MEAL_FLAG_0);
+			od1.setProductCount(2);
+			od1.setGiftFlag(CommonConstant.GIFT_FLAG_1);
 			OrderDetailIn od2 = new OrderDetailIn();
-			od2.setProductCode("10000095");
+			od2.setProductCode("10000140");
 			od2.setMealFlag(CommonConstant.MEAL_FLAG_0);
+			od2.setProductCount(3);
+			od2.setGiftFlag(CommonConstant.GIFT_FLAG_0);
 			OrderDetailIn od3 = new OrderDetailIn();
-			od3.setProductCode("10000098");
+			od3.setProductCode("10000139");
 			od3.setMealFlag(CommonConstant.MEAL_FLAG_0);
+			od3.setProductCount(2);
+			od3.setGiftFlag(CommonConstant.GIFT_FLAG_0);
 			order.getOrderDetailIns().add(od1);
 			order.getOrderDetailIns().add(od2);
 			order.getOrderDetailIns().add(od3);
+			order.setMemberCardNo("100000002");
+			QueryIds.userNo.set("14020");
+			System.err.println(JSON.toJSONString(this.orderRestController.submitOrder(order), true));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testSubmitOrderForMeal() {
+		try {
+			OrderIn order = new OrderIn();
+			OrderDetailIn od1 = new OrderDetailIn();
+			od1.setMealNo("T0000148");
+			od1.setMealFlag(CommonConstant.MEAL_FLAG_1);
+			od1.setGiftFlag(CommonConstant.GIFT_FLAG_0);
+			OrderDetailIn od2 = new OrderDetailIn();
+			od2.setProductCode("10000140");
+			od2.setMealFlag(CommonConstant.MEAL_FLAG_1);
+			od2.setProductCount(3);
+			od2.setGiftFlag(CommonConstant.GIFT_FLAG_0);
+			OrderDetailIn od3 = new OrderDetailIn();
+			od3.setProductCode("10000139");
+			od3.setMealFlag(CommonConstant.MEAL_FLAG_1);
+			od3.setProductCount(2);
+			od3.setGiftFlag(CommonConstant.GIFT_FLAG_0);
+			order.getOrderDetailIns().add(od1);
+//			order.getOrderDetailIns().add(od2);
+//			order.getOrderDetailIns().add(od3);
 			order.setMemberCardNo("100000002");
 			QueryIds.userNo.set("14020");
 			System.err.println(JSON.toJSONString(this.orderRestController.submitOrder(order), true));
