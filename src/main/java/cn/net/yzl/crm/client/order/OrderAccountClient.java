1fbdf5eabe667acb5ca5bd1d58ddf01310763ffb
w-cuileilei <810435611@qq.com>
2021-01-27 14:36:25 +0800

    退款申请管理页面

---
+++ src/main/java/cn/net/yzl/crm/client/order/OrderAccountClient.java
@@ -0,0 +1,26 @@
+package cn.net.yzl.crm.client.order;
+
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.order.model.vo.order.OrderListAccuntDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+//@FeignClient(name = "orderRefund", url = "${api.gateway.url}/orderService/orderRefund")
+@FeignClient(name = "orderRefund", url = "localhost:4455/orderRefund")
+public interface OrderAccountClient {
+
+    //新建退款订单分页查询
+    @GetMapping("v1/getOrderAccountList")
+    ComResponse<Page<OrderListAccuntDTO>> getOrderAccountList(@RequestParam String orderNo,
+                                                              @RequestParam Integer pageNo,
+                                                              @RequestParam Integer pageSize,
+                                                              @RequestParam String nameOrCard,
+                                                              @RequestParam String financialOwnerName,
+                                                              @RequestParam String startTime,
+                                                              @RequestParam String endTime);
+
+}
