42da4f748e98b66a3563c4977cff870ee496cbba
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-01-27 22:49:39 +0800

    修改入参类型

--- src/main/java/cn/net/yzl/crm/service/micservice/member/MemberProductEffectFien.java
+++ src/main/java/cn/net/yzl/crm/service/micservice/member/MemberProductEffectFien.java
@@ -3,15 +3,12 @@ package cn.net.yzl.crm.service.micservice.member;
 
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.crm.customer.dto.member.MemberProductEffectDTO;
-import cn.net.yzl.crm.customer.model.Member;
 import cn.net.yzl.crm.customer.vo.MemberProductEffectSelectVO;
-import cn.net.yzl.crm.customer.vo.MemberProductEffectVO;
+import cn.net.yzl.crm.customer.vo.MemberProductEffectUpdateVO;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public interface MemberProductEffectFien {
 
     @ApiOperation("修改顾客服用效果记录")
     @PostMapping("/v1/batchModifyProductEffect")
-    ComResponse batchModifyProductEffect(@RequestBody List<MemberProductEffectVO> productEffectVOs);
+    ComResponse batchModifyProductEffect(@RequestBody List<MemberProductEffectUpdateVO> productEffectVOs);
 
     @ApiOperation("获取顾客服用效果记录")
     @PostMapping("/v1/getProductEffects")
