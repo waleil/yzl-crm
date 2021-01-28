e39bdb9b95456d9ba5a8606d43566d789e2cd4a0
zhangyongyong <zhangyongyong@yuzhilin.net.cn>
2021-01-28 10:12:01 +0800

    顾客服务旅程

--- src/main/java/cn/net/yzl/crm/dto/member/MemberServiceJourneryDto.java
+++ src/main/java/cn/net/yzl/crm/dto/member/MemberServiceJourneryDto.java
@@ -27,21 +27,6 @@ public class MemberServiceJourneryDto implements Serializable {
     @ApiModelProperty(value = "顾客服务旅程实体信息集合", name = "memberServiceJourneryList")
     private List<MemberServiceJournery> memberServiceJourneryList;
 
+
 }
 
-@ApiModel(value = "MemberServiceJournery", description = "顾客服务旅程实体信息")
-@Data
-class MemberServiceJournery {
-    @ApiModelProperty(value = "顾客卡号", name = "memberCard")
-    private String memberCard;
-    @ApiModelProperty(value = "员工工号", name = "staffNo")
-    private String staffNo;
-    @ApiModelProperty(value = "开始时间(yyyy年MM月dd日)", name = "startTime")
-    @DateTimeFormat(pattern = "yyyy年MM月dd日")
-    private Date startTime;
-    @ApiModelProperty(value = "结束时间(yyyy年MM月dd日)", name = "endTime")
-    @DateTimeFormat(pattern = "yyyy年MM月dd日")
-    private Date endTime;
-    @ApiModelProperty(value = "累计消费", name = "totalPrice")
-    private double totalPrice;
-}
