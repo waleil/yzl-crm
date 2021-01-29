293e8487d6f9974ecb081f81271eb5ad5bca8c72
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-01-29 20:40:35 +0800

    MongoBaseDaoTests.java

--- src/test/java/cn/net/yzl/crm/test/MongoBaseDaoTests.java
+++ src/test/java/cn/net/yzl/crm/test/MongoBaseDaoTests.java
@@ -1,47 +1,47 @@
-package cn.net.yzl.crm.test;
-
-import org.junit.jupiter.api.Test;
-
-import cn.net.yzl.crm.dao.mogo.DemoMogoDAO;
-import cn.net.yzl.crm.dao.mogo.MongoBaseDao;
-
-/**
- * 单元测试类
- * 
- * @author zhangweiwei
- * @date 2021年1月18日,下午7:10:24
- */
-public class MongoBaseDaoTests {
-	@Test
-	public void testGetParamType() {
-		new DemoMogoDAO();
-		new IntegerMogoDao();
-		new StringMogoDao();
-	}
-
-	static class IntegerMogoDao extends MongoBaseDao<Integer> {
-
-		@Override
-		protected Class<Integer> getEntityClass() {
-			return null;
-		}
-	}
-
-	static class StringMogoDao extends MongoBaseDao<String> {
-
-		@Override
-		protected Class<String> getEntityClass() {
-			return null;
-		}
-	}
-
-	// 不建议使用这种写法，既然用了泛型，就需要指定泛型参数类型
-	@SuppressWarnings("rawtypes")
-	static class ObjectMogoDao extends MongoBaseDao {
-
-		@Override
-		protected Class getEntityClass() {
-			return Object.class;
-		}
-	}
-}
+//package cn.net.yzl.crm.test;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import cn.net.yzl.crm.dao.mogo.DemoMogoDAO;
+//import cn.net.yzl.crm.dao.mogo.MongoBaseDao;
+//
+///**
+// * 单元测试类
+// * 
+// * @author zhangweiwei
+// * @date 2021年1月18日,下午7:10:24
+// */
+//public class MongoBaseDaoTests {
+//	@Test
+//	public void testGetParamType() {
+//		new DemoMogoDAO();
+//		new IntegerMogoDao();
+//		new StringMogoDao();
+//	}
+//
+//	static class IntegerMogoDao extends MongoBaseDao<Integer> {
+//
+//		@Override
+//		protected Class<Integer> getEntityClass() {
+//			return null;
+//		}
+//	}
+//
+//	static class StringMogoDao extends MongoBaseDao<String> {
+//
+//		@Override
+//		protected Class<String> getEntityClass() {
+//			return null;
+//		}
+//	}
+//
+//	// 不建议使用这种写法，既然用了泛型，就需要指定泛型参数类型
+//	@SuppressWarnings("rawtypes")
+//	static class ObjectMogoDao extends MongoBaseDao {
+//
+//		@Override
+//		protected Class getEntityClass() {
+//			return Object.class;
+//		}
+//	}
+//}
