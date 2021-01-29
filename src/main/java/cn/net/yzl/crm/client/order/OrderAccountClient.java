89be7e7ef740718a2621c04e3c525a77813e25d9
w-cuileilei <810435611@qq.com>
2021-01-29 14:18:23 +0800

    结算中心付款页面

--- src/main/java/cn/net/yzl/crm/client/order/OrderAccountClient.java
+++ src/main/java/cn/net/yzl/crm/client/order/OrderAccountClient.java
@@ -2,15 +2,15 @@ package cn.net.yzl.crm.client.order;
 
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.order.model.vo.order.OrderAccoundInfoDTO;
 import cn.net.yzl.order.model.vo.order.OrderListAccuntDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Date;
-
-@FeignClient(name = "orderRefund", url = "${api.gateway.url}/orderService/orderRefund")
-//@FeignClient(name = "orderRefund", url = "localhost:4455/orderRefund")
+//@FeignClient(name = "orderRefund", url = "${api.gateway.url}/orderService/orderRefund")
+@FeignClient(name = "orderRefund", url = "localhost:4455/orderRefund")
 public interface OrderAccountClient {
 
     //新建退款订单分页查询
@@ -22,5 +22,16 @@ public interface OrderAccountClient {
                                                               @RequestParam String financialOwnerName,
                                                               @RequestParam String startTime,
                                                               @RequestParam String endTime);
+    //退款商品信息查询
+    @GetMapping("v1/selectAccountOrderInfo")
+    ComResponse<OrderAccoundInfoDTO> selectAccountOrderInfo(@RequestParam String saleOrderNo,@RequestParam String orderNo);
 
+    @PostMapping("v1/saveAccountOrderInfo")
+    ComResponse<Boolean> saveAccountOrderInfo(@RequestParam String refundPayeeAccount,
+                                              @RequestParam String transactionNo,
+                                              @RequestParam String orderAccountTime,
+                                              @RequestParam String saleOrderNo,
+                                              @RequestParam String updateCode,
+                                              @RequestParam String updateName,
+                                              @RequestParam String isn);
 }
