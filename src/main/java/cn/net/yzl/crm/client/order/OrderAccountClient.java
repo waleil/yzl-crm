343cdafd556d8c8dbfb05c5a3b0c913f739ba376
w-cuileilei <810435611@qq.com>
2021-01-29 14:29:03 +0800

    Feign地址修改

--- src/main/java/cn/net/yzl/crm/client/order/OrderAccountClient.java
+++ src/main/java/cn/net/yzl/crm/client/order/OrderAccountClient.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient(name = "orderRefund", url = "${api.gateway.url}/orderService/orderRefund")
-@FeignClient(name = "orderRefund", url = "localhost:4455/orderRefund")
+@FeignClient(name = "orderRefund", url = "${api.gateway.url}/orderService/orderRefund")
+//@FeignClient(name = "orderRefund", url = "localhost:4455/orderRefund")
 public interface OrderAccountClient {
 
     //新建退款订单分页查询
