ce26541cb99d8191fdf3432f9c3d7e5824e494ae
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-01-30 22:56:27 +0800

    OrderSaleClient.java

--- src/main/java/cn/net/yzl/crm/client/order/OrderSaleClient.java
+++ src/main/java/cn/net/yzl/crm/client/order/OrderSaleClient.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -93,5 +94,6 @@ public interface OrderSaleClient {
      * @param dto
      * @return
      */
-    ComResponse<Boolean> reviewSaleOrder(SaleOrderReviewDTO dto);
+    @PutMapping("/v1/reviewSaleOrder")
+    ComResponse<Boolean> reviewSaleOrder(@RequestBody SaleOrderReviewDTO dto);
 }
