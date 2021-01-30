14993f139eaaff7c791961f17332ed868befeb52
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-01-31 01:49:11 +0800

    OrderRestController.java

--- src/main/java/cn/net/yzl/crm/controller/order/OrderRestController.java
+++ src/main/java/cn/net/yzl/crm/controller/order/OrderRestController.java
@@ -596,6 +596,7 @@ public class OrderRestController {
 				od.setSpec(String.valueOf(p.getTotalUseNum()));// 商品规格
 				od.setPackageunit(p.getPackagingUnit());// 包装单位
 				productStockMap.put(od.getProductCode(), p.getStock());// 库存
+				tuples.add(new Tuple(od.getProductCode(), od.getProductCount()));// 商品总数
 				// 如果是非赠品
 				if (CommonConstant.GIFT_FLAG_0.equals(od.getGiftFlag())) {
 					od.setTotal(od.getProductUnitPrice() * od.getProductCount());// 实收金额，单位分
