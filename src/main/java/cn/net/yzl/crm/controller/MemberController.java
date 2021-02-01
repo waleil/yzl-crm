f8f1947e38228c8b1ab91478029c84cef8f65d29
lichanghong <lichanghong@yuzhilin.net.cn>
2021-02-01 18:20:21 +0800

    修改返回类型

--- src/main/java/cn/net/yzl/crm/controller/MemberController.java
+++ src/main/java/cn/net/yzl/crm/controller/MemberController.java
@@ -102,13 +102,13 @@ public class MemberController {
 
     @ApiOperation("获取顾客联系方式信息，包括手机号，座机号")
     @GetMapping("/v1/getMemberPhoneList")
-    public GeneralResult getMemberPhoneList(
+    public GeneralResult<List<MemberPhone>> getMemberPhoneList(
             @RequestParam("member_card")
             @NotBlank(message = "member_card不能为空")
             @ApiParam(name = "member_card", value = "会员卡号", required = true)
                     String member_card) {
         GeneralResult<List<MemberPhone>> result = memberFien.getMemberPhoneList(member_card);
-        return GeneralResult.success(result);
+        return result;
     }
 
     /**
@@ -120,7 +120,7 @@ public class MemberController {
     @ApiOperation("根据手机号获取顾客信息（可用来判断手机号是否被注册，如果被注册则返回注册顾客实体）")
     //@GetMapping("/v1/getMemberByPhone")
     @GetMapping("/v1/getMemberByphoneNumber")
-    public ComResponse getMemberByPhone(
+    public ComResponse<Member> getMemberByPhone(
             @RequestParam("phone")
             @NotBlank(message = "phone不能为空")
             @ApiParam(name = "phone", value = "手机号", required = true)
@@ -154,7 +154,7 @@ public class MemberController {
      */
     @ApiOperation("获取顾客购买商品")
     @GetMapping("v1/getMemberProductEffectList")
-    public GeneralResult getMemberProductEffectList(
+    public GeneralResult<List<MemberProductEffect>> getMemberProductEffectList(
             @RequestParam("member_card")
             @NotBlank(message = "member_card不能为空")
             @ApiParam(name = "member_card", value = "会员卡号", required = true)
@@ -171,7 +171,7 @@ public class MemberController {
      */
     @ApiOperation("获取顾客咨询商品")
     @GetMapping("v1/getProductConsultationList")
-    public GeneralResult getProductConsultationList(
+    public GeneralResult<List<ProductConsultation>> getProductConsultationList(
             @RequestParam("member_card")
             @NotBlank(message = "member_card不能为空")
             @ApiParam(name = "member_card", value = "会员卡号", required = true)
