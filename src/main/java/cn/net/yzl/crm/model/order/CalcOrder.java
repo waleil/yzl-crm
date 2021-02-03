ff680f09f2b1b38bd854242496e1da25022d8741
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-03 17:06:43 +0800

    CalcOrder.java

---
+++ src/main/java/cn/net/yzl/crm/model/order/CalcOrder.java
@@ -0,0 +1,25 @@
+package cn.net.yzl.crm.model.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.net.yzl.order.model.vo.order.OrderDetailIn;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 订单
+ * 
+ * @author zhangweiwei
+ * @date 2021年2月3日,下午4:22:32
+ */
+@ApiModel(description = "订单")
+public class CalcOrder {
+
+	@ApiModelProperty(value = "使用储值金额 单位元", required = false)
+	private Double amountStored;
+	@ApiModelProperty(value = "使用优惠券 单位元", required = false)
+	private Double amountCoupon;
+	@ApiModelProperty(value = "订单明细", required = true)
+	private List<OrderDetailIn> orderDetailIns = new ArrayList<OrderDetailIn>(0);
+}
