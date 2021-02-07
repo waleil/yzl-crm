126925888727f8b1069c5b3aacd889cdd0f77f8d
w-cuileilei <810435611@qq.com>
2021-02-07 19:06:36 +0800

    财务归属

---
+++ src/main/java/cn/net/yzl/crm/client/order/OrderFinanceClient.java
@@ -0,0 +1,23 @@
+package cn.net.yzl.crm.client.order;
+
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.order.model.db.order.OrderFinance;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+//@FeignClient(name = "OrderFinanceClient", url = "${api.gateway.url}/staffDB")
+@FeignClient(name = "OrderFinanceClient", url = "localhost:38080/")
+public interface OrderFinanceClient {
+
+    /**
+     * 财务归属
+     *
+     * @param
+     * @return
+     */
+    @GetMapping("/depart/getAllFinanceDepart")
+
+    ComResponse<List<OrderFinance>> getOrderFinanceList();
+}
