6166100960c6d5fb70290f0a45e6ef6a15fce546
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:20:36 +0800

    MemberCustomerJourneyDto.java

--- src/main/java/cn/net/yzl/crm/dto/member/customerJourney/MemberCustomerJourneyDto.java
+++ src/main/java/cn/net/yzl/crm/dto/member/customerJourney/MemberCustomerJourneyDto.java
@@ -9,10 +9,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
