b7ea6ee63deeba18bc551496329db6af6a16b4b7
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:19:38 +0800

    ActivityController.java

--- src/main/java/cn/net/yzl/crm/controller/ActivityController.java
+++ src/main/java/cn/net/yzl/crm/controller/ActivityController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 /**
  * @author: liuChangFu
