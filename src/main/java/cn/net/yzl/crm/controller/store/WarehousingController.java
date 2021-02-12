acf4073352eba91f9cea038bde1a957ead6eb539
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 22:15:46 +0800

    WarehousingController.java

--- src/main/java/cn/net/yzl/crm/controller/store/WarehousingController.java
+++ src/main/java/cn/net/yzl/crm/controller/store/WarehousingController.java
@@ -5,7 +5,6 @@ import cn.net.yzl.crm.client.store.WarehousingFeignService;
 import cn.net.yzl.model.vo.WarehousingSelectVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +18,6 @@ import java.util.List;
  * @description:
  */
 @RestController
-@Slf4j
 @Api(value = "仓储中心-入库单", tags = {"仓储中心-入库单"})
 @RequestMapping("warehousing")
 public class WarehousingController {
