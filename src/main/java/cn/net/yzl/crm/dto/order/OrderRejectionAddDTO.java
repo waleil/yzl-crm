0ad0728703541fbf3509fd20b770b572553e7bee
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:21:44 +0800

    OrderRejectionAddDTO.java

--- src/main/java/cn/net/yzl/crm/dto/order/OrderRejectionAddDTO.java
+++ src/main/java/cn/net/yzl/crm/dto/order/OrderRejectionAddDTO.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
