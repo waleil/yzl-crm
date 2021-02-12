48c00ec13b5e280e594a3848fbd60981fb4cafd4
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:22:34 +0800

    MealRequestVO.java

--- src/main/java/cn/net/yzl/crm/model/MealRequestVO.java
+++ src/main/java/cn/net/yzl/crm/model/MealRequestVO.java
@@ -8,7 +8,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 import java.util.List;
 
