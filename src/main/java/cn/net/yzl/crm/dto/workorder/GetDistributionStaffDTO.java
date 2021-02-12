579873774b739b65cb5aa97986b28dedbbfd9d23
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:31:44 +0800

    GetDistributionStaffDTO.java

--- src/main/java/cn/net/yzl/crm/dto/workorder/GetDistributionStaffDTO.java
+++ src/main/java/cn/net/yzl/crm/dto/workorder/GetDistributionStaffDTO.java
@@ -2,8 +2,10 @@ package cn.net.yzl.crm.dto.workorder;
 
 import cn.net.yzl.workorder.model.dto.BaseDTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class GetDistributionStaffDTO extends BaseDTO<GetDistributionStaffDTO> {
 
     private String staffNo;
