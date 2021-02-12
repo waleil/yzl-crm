240d10c0eaae6c4edf50747db1de3d36555d4bdd
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:21:54 +0800

    GetDistributionStaffDTO.java

--- src/main/java/cn/net/yzl/crm/dto/workorder/GetDistributionStaffDTO.java
+++ src/main/java/cn/net/yzl/crm/dto/workorder/GetDistributionStaffDTO.java
@@ -1,10 +1,7 @@
 package cn.net.yzl.crm.dto.workorder;
 
 import cn.net.yzl.workorder.model.dto.BaseDTO;
-import io.swagger.annotations.ApiModelProperty;
-import javax.validation.constraints.NotNull;
 import lombok.Data;
-import org.springframework.validation.annotation.Validated;
 
 @Data
 public class GetDistributionStaffDTO extends BaseDTO<GetDistributionStaffDTO> {
