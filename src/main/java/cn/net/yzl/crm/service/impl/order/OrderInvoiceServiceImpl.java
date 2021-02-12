c4dc7b83510f17e74ff1cc52b19c2b1dda95cb0e
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:23:00 +0800

    OrderInvoiceServiceImpl.java

--- src/main/java/cn/net/yzl/crm/service/impl/order/OrderInvoiceServiceImpl.java
+++ src/main/java/cn/net/yzl/crm/service/impl/order/OrderInvoiceServiceImpl.java
@@ -13,7 +13,6 @@ import cn.net.yzl.order.model.vo.order.OrderInvoiceReqDTO;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -21,13 +20,10 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
