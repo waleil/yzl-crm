7738d7ad75b4635cc80883837b84d35668f56a8e
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-12 21:18:02 +0800

    OrderAccountController.java

--- src/main/java/cn/net/yzl/crm/controller/order/OrderAccountController.java
+++ src/main/java/cn/net/yzl/crm/controller/order/OrderAccountController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
