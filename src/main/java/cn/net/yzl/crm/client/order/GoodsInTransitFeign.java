0f8e4744d3c514e7d1b361624a27e2d00c632bf6
chengyu <cy091206>
2021-02-23 02:24:46 +0800

    在途商品

---
+++ src/main/java/cn/net/yzl/crm/client/order/GoodsInTransitFeign.java
@@ -0,0 +1,31 @@
+package cn.net.yzl.crm.client.order;
+
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.order.model.mongo.order.GoodsInTransit;
+import cn.net.yzl.order.model.mongo.order.ProductDetailDTO;
+import cn.net.yzl.order.model.vo.order.GoodsInTransitReqDTO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "newOrder",url = "${api.gateway.url}/orderService/goodsintransit")
+//@FeignClient(name = "goodsintransit",url = "localhost:4455/goodsintransit")
+public interface GoodsInTransitFeign {
+
+
+
+    @ApiOperation(value = "查询在途商品明细")
+    @PostMapping("v1/goodsInTransitdetails")
+    public ComResponse<Page<ProductDetailDTO>>  goodsInTransitdetail (@RequestBody GoodsInTransitReqDTO dto );
+
+    @ApiOperation(value = "查询在途商品")
+    @PostMapping("v1/selectgoodsInTransitlist")
+    public ComResponse<Page<GoodsInTransit>>  selectgoodsInTransitlist (@RequestBody GoodsInTransitReqDTO dto );
+
+    @ApiOperation(value = "查询在途商品总数量")
+    @PostMapping("v1/selectgoodsInTransitTotalCount")
+    public ComResponse<Integer>  selectgoodsInTransitTotalCount (@RequestBody GoodsInTransitReqDTO dto );
+
+}
