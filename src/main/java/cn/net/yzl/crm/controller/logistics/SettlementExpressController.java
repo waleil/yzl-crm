873a027990f4ce7d8e43925373284ccd0fa3b621
liuguangjie <liuguangjie@yuzhilin.net.cn>
2021-02-22 15:04:54 +0800

    dev

---
+++ src/main/java/cn/net/yzl/crm/controller/logistics/SettlementExpressController.java
@@ -0,0 +1,41 @@
+package cn.net.yzl.crm.controller.logistics;
+
+
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.enums.ResponseCodeEnums;
+import cn.net.yzl.crm.service.micservice.LogisticsFien;
+import cn.net.yzl.logistics.model.vo.ImportResult;
+import cn.net.yzl.order.model.vo.order.ImportParam;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("settlement")
+@Api(tags = "结算API")
+public class SettlementExpressController {
+
+    @Autowired
+    private LogisticsFien settlement;
+
+    @PostMapping("import/settlement/freight")
+    @ApiOperation("快递费用导入")
+    public ComResponse<ImportResult> importSettlement(@RequestBody ImportParam param){
+
+        if (!StringUtils.hasText(param.getExpressCompanyCode())) {
+            return ComResponse.fail(ResponseCodeEnums.ERROR, "快递公司编码不能为空");
+        }
+        if (!StringUtils.hasText(param.getFileUrl())) {
+            return ComResponse.fail(ResponseCodeEnums.ERROR, "上传文件URL不能为空");
+        }
+        return this.settlement.importFromExcel(param);
+
+    }
+}
