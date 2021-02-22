9b73347ecaf5198008aaae6e8bf4db7dc6eed413
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-02-23 00:12:35 +0800

    格式化时间

--- src/main/java/cn/net/yzl/crm/dto/workorder/MemberFirstCallDetailsDTO.java
+++ src/main/java/cn/net/yzl/crm/dto/workorder/MemberFirstCallDetailsDTO.java
@@ -48,7 +48,7 @@ public class MemberFirstCallDetailsDTO implements Serializable {
     private String phone;
 
     @ApiModelProperty(value = "记录生成时间")
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
     @ApiModelProperty(value = "呼入时间列表")
