f16d74f5cfa8d8b503481539ff2fd3d1f7612012
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-02-24 13:39:20 +0800

    ProductClientTests.java

---
+++ src/test/java/cn/net/yzl/crm/test/ProductClientTests.java
@@ -0,0 +1,59 @@
+package cn.net.yzl.crm.test;
+
+import java.util.Arrays;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import cn.net.yzl.crm.client.product.ProductClient;
+import cn.net.yzl.product.model.vo.product.vo.OrderProductVO;
+import cn.net.yzl.product.model.vo.product.vo.ProductReduceVO;
+
+/**
+ * 单元测试类
+ * 
+ * @author zhangweiwei
+ * @date 2021年2月24日,下午1:38:21
+ */
+@SpringBootTest
+public class ProductClientTests {
+	@Resource
+	private ProductClient productClient;
+
+	@Test
+	public void testQueryByCodes() {
+		try {
+			String codes = "10000156,10000155,10000152";
+			this.productClient.queryByProductCodes(codes).getData().forEach(System.err::println);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testProductReduce() {
+		try {
+			OrderProductVO vo = new OrderProductVO();
+			vo.setOrderNo("ON1314020T202101271816500065");
+			ProductReduceVO p1 = new ProductReduceVO();
+			p1.setNum(1);
+			p1.setOrderNo(vo.getOrderNo());
+			p1.setProductCode("10000145");
+			ProductReduceVO p2 = new ProductReduceVO();
+			p2.setNum(1);
+			p2.setOrderNo(vo.getOrderNo());
+			p2.setProductCode("10000144");
+			ProductReduceVO p4 = new ProductReduceVO();
+			p4.setNum(1);
+			p4.setOrderNo(vo.getOrderNo());
+			p4.setProductCode("10000139");
+			vo.setProductReduceVOS(Arrays.asList(p1, p2, p4));
+			System.err.println(this.productClient.productReduce(vo));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
