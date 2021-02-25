fd1316f4fccc9d12021d9633dbd9fa67e0015517
w-majinbao <w-majinbao@yuzhilin.net.cn>
2021-02-25 21:28:13 +0800

    score crm
    # Conflicts:
    #	src/main/java/cn/net/yzl/crm/client/score/ScoreProductClient.java
    #	src/main/java/cn/net/yzl/crm/controller/score/ScoreController.java
    #	src/main/java/cn/net/yzl/crm/service/score/ScoreService.java
    #	src/main/java/cn/net/yzl/crm/service/score/impl/ScoreServiceImpl.java

--- src/main/java/cn/net/yzl/crm/client/score/ScoreProductClient.java
+++ src/main/java/cn/net/yzl/crm/client/score/ScoreProductClient.java
@@ -1,50 +1,34 @@
 package cn.net.yzl.crm.client.score;
 
-import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.common.entity.Page;
-import cn.net.yzl.crm.client.product.ProductClient;
-import cn.net.yzl.score.model.dto.MyExchangeRecordDTO;
-import cn.net.yzl.score.model.dto.ScoreProductDetailDTO;
-import cn.net.yzl.score.model.dto.ScoreProductMainInfoDTO;
-import cn.net.yzl.score.model.vo.ChangeProductStatusVO;
-import cn.net.yzl.score.model.vo.ExchangeVO;
-import cn.net.yzl.score.model.vo.ProductDelVO;
-import cn.net.yzl.score.model.vo.ScoreProductVO;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.Valid;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.net.yzl.common.entity.ComResponse;
+import cn.net.yzl.common.entity.Page;
+import cn.net.yzl.score.model.dto.ScoreProductDetailDTO;
+import cn.net.yzl.score.model.dto.ScoreProductMainInfoDTO;
+import cn.net.yzl.score.model.vo.ScoreProductVO;
+
 @FeignClient(name = "scoreProductClient", url = "${api.gateway.url}/scoreServer/scoreProduct/v1")
-//@FeignClient(name = "scoreProductClient", url = "127.0.0.1:8765/scoreProduct/v1")
 public interface ScoreProductClient {
 
 //    @RequestMapping(value = "uploadScoreProductFile", method = RequestMethod.POST)
 //    ComResponse<String> uploadScoreProductFile(@RequestParam(value = "file") MultipartFile file);
 
     @GetMapping("queryPage")
-    ComResponse<Page<ScoreProductMainInfoDTO>> queryPage(@RequestParam("pageSize") Integer pageSize,
-                                                         @RequestParam("pageNo") Integer pageNo,
-                                                         @RequestParam("hide") Boolean hide);
+    ComResponse<Page<ScoreProductMainInfoDTO>> queryPage(@RequestParam("pageSize")Integer pageSize, @RequestParam("pageNo")Integer pageNo);
 
     @GetMapping("queryDetail")
     ComResponse<ScoreProductDetailDTO> queryDetail(@RequestParam("id")Integer id);
 
     @PostMapping("edit")
-    ComResponse<Void> edit(@RequestBody ScoreProductVO vo);
+    ComResponse<Void> edit(@RequestBody @Valid ScoreProductVO vo);
 
     @PostMapping("delete")
-    ComResponse<Void> delete(@RequestBody ProductDelVO vo);
-
-    @PostMapping("exchange")
-    ComResponse<Void> exchange(@RequestBody ExchangeVO vo);
-
-    @GetMapping("myScore")
-    ComResponse<Integer> myScore(@RequestParam("staffNo") String staffNo);
-
-    @PostMapping("changeStatus")
-    ComResponse<Void> changeStatus(@RequestBody ChangeProductStatusVO vo);
-
+    ComResponse<Void> delete(@RequestParam("id") Integer id, @RequestParam("userNo") String userNo);
 }
