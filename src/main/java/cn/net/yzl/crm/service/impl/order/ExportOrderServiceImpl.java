21ada90d05557c752ea5432b4ac89ba4b965242b
程宇 <chengyu@yuzhilin.net.cn>
2021-02-27 21:07:28 +0800

    商品销售明细导出

--- src/main/java/cn/net/yzl/crm/service/impl/order/ExportOrderServiceImpl.java
+++ src/main/java/cn/net/yzl/crm/service/impl/order/ExportOrderServiceImpl.java
@@ -10,6 +10,7 @@ import cn.net.yzl.crm.client.order.OrderSearchClient;
 import cn.net.yzl.crm.client.order.SettlementFein;
 import cn.net.yzl.crm.service.order.ExportOrderService;
 import cn.net.yzl.crm.service.order.IOrderSearchService;
+import cn.net.yzl.crm.sys.BizException;
 import cn.net.yzl.order.constant.CommonConstant;
 import cn.net.yzl.order.model.mongo.order.GoodsInTransit;
 import cn.net.yzl.order.model.vo.order.*;
@@ -31,6 +32,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -55,20 +57,28 @@ public class ExportOrderServiceImpl implements ExportOrderService {
         ComResponse<Page<SettlementDTO>>  data = this.settlementFein.settlementList(dto);
         if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
             log.error("导出结算列表异常>>>{}", data);
-            return;
-        }
-        Page<SettlementDTO> page = data.getData();
-        PageParam param = page.getPageParam();
-        if (param.getPageTotal() == 0) {
-            log.info("结算列表为空>>>{}", param);
-            return;
+            throw new BizException(ResponseCodeEnums.API_ERROR_CODE.getCode(),
+                    "调用导出在途商品类表异常"+data.getMessage());
         }
         response.setContentType("application/vnd.ms-excel");
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        String title = "结算列表";
+
+        String title = new String("结算列表".getBytes(),StandardCharsets.UTF_8.name());
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment;filename=%s%s.xlsx",
                 URLEncoder.encode(title, StandardCharsets.UTF_8.name()), System.currentTimeMillis()));
 
+        Page<SettlementDTO> page = data.getData();
+        PageParam param = page.getPageParam();
+        if (param.getPageTotal() == 0) {
+            excelWriter = EasyExcel.write(response.getOutputStream(), Settlement4ExportDTO.class)
+                    .registerWriteHandler(this.writeHandler).build();
+            // 写入到同一个sheet
+            WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+            // 此处已经获取到第一页的数据
+
+            excelWriter.write(Arrays.asList(new Settlement4ExportDTO()), writeSheet);
+
+        }else{
 
             excelWriter = EasyExcel.write(response.getOutputStream(), Settlement4ExportDTO.class)
                     .registerWriteHandler(this.writeHandler).build();
@@ -96,6 +106,9 @@ public class ExportOrderServiceImpl implements ExportOrderService {
                     page.getItems().clear();// 存储完成后清理集合
                 }
             }
+        }
+
+
         }catch(Exception e){
             log.error(e.getMessage(),e);
         }finally {
@@ -115,7 +128,8 @@ public class ExportOrderServiceImpl implements ExportOrderService {
         try {
             response.setContentType("application/vnd.ms-excel");
             response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-            String title = "已结算商品列表";
+
+            String title = new String("已结算商品列表".getBytes(),StandardCharsets.UTF_8.name());
             response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment;filename=%s%s.xlsx",
                     URLEncoder.encode(title, StandardCharsets.UTF_8.name()), System.currentTimeMillis()));
 
@@ -124,38 +138,47 @@ public class ExportOrderServiceImpl implements ExportOrderService {
             ComResponse<Page<ProductDetailSettlementedResDTO>> data = this.settlementFein.selectProductDetailBySettledOrder(dto);
             if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
                 log.error("导出已结算商品列表异常>>>{}", data);
-                return;
+                throw new BizException(ResponseCodeEnums.API_ERROR_CODE.getCode(),
+                        "调用导出在途商品类表异常"+data.getMessage());
             }
             Page<ProductDetailSettlementedResDTO> page = data.getData();
             PageParam param = page.getPageParam();
             if (param.getPageTotal() == 0) {
-                log.info("已结算商品列表空>>>{}", param);
-                return;
-            }
+                excelWriter = EasyExcel.write(response.getOutputStream(), ProductDetailSettlementedResDTO.class)
+                        .registerWriteHandler(this.writeHandler).build();
+                // 写入到同一个sheet
+                WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+                // 此处已经获取到第一页的数据
 
+                excelWriter.write(page.getItems(), writeSheet);
 
-            excelWriter = EasyExcel.write(response.getOutputStream(), ProductDetailSettlementedResDTO.class)
-                    .registerWriteHandler(this.writeHandler).build();
-            // 写入到同一个sheet
-            WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
-            // 此处已经获取到第一页的数据
-            excelWriter.write(data.getData().getItems(), writeSheet);
-            page.getItems().clear();// 存储完成后清理集合
-            // 如果总页数大于1
-            if (param.getPageTotal() > 1) {
-                // 直接从第二页开始获取
-                for (int i = 2; i <= param.getPageTotal(); i++) {
-                    dto.setPageNo(i);
-                    data = this.settlementFein.selectProductDetailBySettledOrder(dto);
-                    if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
-                        log.error("导出结算列表异常>>>{}", data);
-                        return;
+            }else{
+
+                excelWriter = EasyExcel.write(response.getOutputStream(), ProductDetailSettlementedResDTO.class)
+                        .registerWriteHandler(this.writeHandler).build();
+                // 写入到同一个sheet
+                WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+                // 此处已经获取到第一页的数据
+                excelWriter.write(data.getData().getItems(), writeSheet);
+                page.getItems().clear();// 存储完成后清理集合
+                // 如果总页数大于1
+                if (param.getPageTotal() > 1) {
+                    // 直接从第二页开始获取
+                    for (int i = 2; i <= param.getPageTotal(); i++) {
+                        dto.setPageNo(i);
+                        data = this.settlementFein.selectProductDetailBySettledOrder(dto);
+                        if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
+                            log.error("导出结算列表异常>>>{}", data);
+                            return;
+                        }
+                        page = data.getData();
+                        excelWriter.write(page.getItems(), writeSheet);
+                        page.getItems().clear();// 存储完成后清理集合
                     }
-                    page = data.getData();
-                    excelWriter.write(page.getItems(), writeSheet);
-                    page.getItems().clear();// 存储完成后清理集合
                 }
             }
+
+
         }catch(Exception e){
             log.error(e.getMessage(),e);
         }finally {
@@ -171,49 +194,62 @@ public class ExportOrderServiceImpl implements ExportOrderService {
         try {
             dto.setPageNo(1);// 默认第1页
             dto.setPageSize(1000);// 默认每页1000条数据
+
             ComResponse<Page<GoodsInTransit>> data = this.goodsInTransitFeign.selectgoodsInTransitlist(dto);
 
             if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
                 log.error("导出在途商品列表异常>>>{}", data);
-                return;
+                throw new BizException(ResponseCodeEnums.API_ERROR_CODE.getCode(),
+                        "调用导出在途商品类表异常"+data.getMessage());
             }
+            response.setContentType("application/vnd.ms-excel");
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+            String title = new String("在途商品列表".getBytes(),StandardCharsets.UTF_8.name());
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment;filename=%s%s.xlsx",
+                    URLEncoder.encode(title, StandardCharsets.UTF_8.name()), System.currentTimeMillis()));
+
             Page<GoodsInTransit> page = data.getData();
             PageParam param = page.getPageParam();
             if (param.getPageTotal() == 0) {
                 log.info("在途商品列表为空>>>{}", param);
-                return;
-            }
-            response.setContentType("application/vnd.ms-excel");
-            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-            String title = "在途商品列表";
-            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment;filename=%s%s.xlsx",
-                    URLEncoder.encode(title, StandardCharsets.UTF_8.name()), System.currentTimeMillis()));
+                excelWriter = EasyExcel.write(response.getOutputStream(), GoodsInTransit.class)
+                        .registerWriteHandler(this.writeHandler).build();
+                // 写入到同一个sheet
+                WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+                // 此处已经获取到第一页的数据
 
+                excelWriter.write(page.getItems(), writeSheet);
+            }else{
 
-            excelWriter = EasyExcel.write(response.getOutputStream(), GoodsInTransit.class)
-                    .registerWriteHandler(this.writeHandler).build();
-            // 写入到同一个sheet
-            WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
-            // 此处已经获取到第一页的数据
+                excelWriter = EasyExcel.write(response.getOutputStream(), GoodsInTransit.class)
+                        .registerWriteHandler(this.writeHandler).build();
+                // 写入到同一个sheet
+                WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+                // 此处已经获取到第一页的数据
 
-            excelWriter.write(page.getItems(), writeSheet);
-            page.getItems().clear();// 存储完成后清理集合
-            // 如果总页数大于1
-            if (param.getPageTotal() > 1) {
-                // 直接从第二页开始获取
-                for (int i = 2; i <= param.getPageTotal(); i++) {
-                    dto.setPageNo(i);
-                    data = this.goodsInTransitFeign.selectgoodsInTransitlist(dto);
-                    if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
-                        log.error("导出在途商品列表异常>>>{}", data);
-                        return;
+                excelWriter.write(page.getItems(), writeSheet);
+                page.getItems().clear();// 存储完成后清理集合
+                // 如果总页数大于1
+                if (param.getPageTotal() > 1) {
+                    // 直接从第二页开始获取
+                    for (int i = 2; i <= param.getPageTotal(); i++) {
+                        dto.setPageNo(i);
+                        data = this.goodsInTransitFeign.selectgoodsInTransitlist(dto);
+                        if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
+                            log.error("导出在途商品列表异常>>>{}", data);
+                            return;
+                        }
+                        page = data.getData();
+
+                        excelWriter.write(page.getItems(), writeSheet);
+                        page.getItems().clear();// 存储完成后清理集合
                     }
-                    page = data.getData();
-
-                    excelWriter.write(page.getItems(), writeSheet);
-                    page.getItems().clear();// 存储完成后清理集合
                 }
             }
+
+
+
         }catch(Exception e){
             log.error(e.getMessage(),e);
         }finally {
@@ -232,45 +268,55 @@ public class ExportOrderServiceImpl implements ExportOrderService {
 
             if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
                 log.error("导出订单销售明细列表异常>>>{}", data);
-                return;
-            }
-            Page<OrderSellDetailResDTO> page  = data.getData();
-            PageParam param = page.getPageParam();
-            if (param.getPageTotal() == 0) {
-                log.info("订单销售明细列表为空>>>{}", param);
-                return;
+                throw new BizException(ResponseCodeEnums.API_ERROR_CODE.getCode(),
+                        "调用导出订单销售明细列表异常"+data.getMessage());
             }
             response.setContentType("application/vnd.ms-excel");
             response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-            String title = "订单销售明细列表";
+            String title = new String("订单销售明细列表".getBytes(),StandardCharsets.UTF_8.name());
             response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment;filename=%s%s.xlsx",
                     URLEncoder.encode(title, StandardCharsets.UTF_8.name()), System.currentTimeMillis()));
 
 
-            excelWriter = EasyExcel.write(response.getOutputStream(), OrderSellDetailResDTO.class)
-                    .registerWriteHandler(this.writeHandler).build();
-            // 写入到同一个sheet
-            WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
-            // 此处已经获取到第一页的数据
+            Page<OrderSellDetailResDTO> page  = data.getData();
+            PageParam param = page.getPageParam();
+            if (param.getPageTotal() == 0) {
+                log.info("订单销售明细列表为空>>>{}", param);
+                excelWriter = EasyExcel.write(response.getOutputStream(), OrderSellDetailResDTO.class)
+                        .registerWriteHandler(this.writeHandler).build();
+                // 写入到同一个sheet
+                WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+                // 此处已经获取到第一页的数据
+                excelWriter.write(page.getItems(), writeSheet);
 
-            excelWriter.write(page.getItems(), writeSheet);
-            page.getItems().clear();// 存储完成后清理集合
-            // 如果总页数大于1
-            if (param.getPageTotal() > 1) {
-                // 直接从第二页开始获取
-                for (int i = 2; i <= param.getPageTotal(); i++) {
-                    dto.setPageNo(i);
-                    data = this.orderSearchClient.selectOrderSaleDetail(dto);
-                    if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
-                        log.error("导出订单销售明细列表异常>>>{}", data);
-                        return;
+            }else{
+                excelWriter = EasyExcel.write(response.getOutputStream(), OrderSellDetailResDTO.class)
+                        .registerWriteHandler(this.writeHandler).build();
+                // 写入到同一个sheet
+                WriteSheet writeSheet = EasyExcel.writerSheet(title).build();
+                // 此处已经获取到第一页的数据
+
+                excelWriter.write(page.getItems(), writeSheet);
+                page.getItems().clear();// 存储完成后清理集合
+                // 如果总页数大于1
+                if (param.getPageTotal() > 1) {
+                    // 直接从第二页开始获取
+                    for (int i = 2; i <= param.getPageTotal(); i++) {
+                        dto.setPageNo(i);
+                        data = this.orderSearchClient.selectOrderSaleDetail(dto);
+                        if (!ResponseCodeEnums.SUCCESS_CODE.getCode().equals(data.getCode())) {
+                            log.error("导出订单销售明细列表异常>>>{}", data);
+                            return;
+                        }
+                        page = data.getData();
+
+                        excelWriter.write(page.getItems(), writeSheet);
+                        page.getItems().clear();// 存储完成后清理集合
                     }
-                    page = data.getData();
-
-                    excelWriter.write(page.getItems(), writeSheet);
-                    page.getItems().clear();// 存储完成后清理集合
                 }
             }
+
+
         }catch(Exception e){
             log.error(e.getMessage(),e);
         }finally {
