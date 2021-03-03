27f77aade9ca353bb5ea160d1b9f27afbd904a85
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-03-03 16:52:23 +0800

    AuthInterceptor.java

--- src/main/java/cn/net/yzl/crm/config/AuthInterceptor.java
+++ src/main/java/cn/net/yzl/crm/config/AuthInterceptor.java
@@ -21,29 +21,14 @@ import java.io.PrintWriter;
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
-//    @Autowired
-//    private NacosValue nacosValue;
 
     @Autowired
     private IgnoreUrlConfig ignoreUrlConfig;
 
-//    @Autowired
-//    private OauthRemoteService oauthRemoteService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
-//        String serverGatewayUrl = nacosValue.getServerGatewayUrl();
-//        if (!"noCheck".equalsIgnoreCase(serverGatewayUrl)) {
-//            String proto = request.getHeader("x-forwarded-proto");
-//            String host = request.getHeader("x-forwarded-host");
-//            String forwardedUrl = proto + "://" + host;
-//            if (StringUtils.isBlank(host) || !forwardedUrl.equalsIgnoreCase(serverGatewayUrl)) {
-//                log.error("host:[{}],非法请求！", host);
-//                returnJson(response, ComResponse.fail(ResponseCodeEnums.AUTHOR_ERROR_CODE));
-//                return false;
-//            }
-//        }
 
         String spanId = UUIDGenerator.getUUID();
         QueryIds.tranceId.set (request.getHeader("traceId"));
@@ -72,12 +57,6 @@ public class AuthInterceptor implements HandlerInterceptor {
             returnJson(response, ComResponse.fail(ResponseCodeEnums.TOKEN_INVALID_ERROR_CODE.getCode(),ResponseCodeEnums.TOKEN_INVALID_ERROR_CODE.getMessage()));
             return false;
         }
-//        var generalResult = oauthRemoteService.verifyToken(token);
-//        if (HttpStatus.OK.value() != generalResult.getCode()) {
-//            log.error("token:[{}],校验失败:[{}]", token, generalResult.getMessage());
-//            returnJson(response, ComResponse.fail(ResponseCodeEnums.LOGIN_ERROR_CODE));
-//            return false;
-//        }
         return true;
     }
 
