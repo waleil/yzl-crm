9fc783111330bc2e6c046f89afcb6f010317b9d1
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-03-07 15:03:52 +0800

    IOrderOprServiceTests.java

--- src/test/java/cn/net/yzl/crm/test/IOrderOprServiceTests.java
+++ src/test/java/cn/net/yzl/crm/test/IOrderOprServiceTests.java
@@ -5,6 +5,8 @@ import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import cn.net.yzl.crm.service.order.IOrderOprService;
 import cn.net.yzl.order.model.vo.order.OrderCheckDetailDTO;
 
@@ -18,12 +20,35 @@ import cn.net.yzl.order.model.vo.order.OrderCheckDetailDTO;
 public class IOrderOprServiceTests {
 	@Resource
 	private IOrderOprService orderOprService;
+	@Resource
+	private ObjectMapper objectMapper;
 
 	@Test
 	public void testCheckOrder() {
 		try {
 			OrderCheckDetailDTO dto = new OrderCheckDetailDTO();
-			System.err.println(this.orderOprService.checkOrder(dto));
+			dto.setCheckDepartId("");
+			dto.setCheckStatus(1);
+			dto.setCheckUserName("");
+			dto.setCheckUserNo("");
+			dto.setContractContent("");
+			dto.setContractOpinion("");
+			dto.setDepartId("");
+			dto.setOrderNo("ON202103062200014772826057");
+			dto.setRefuseReason("");
+			dto.setRemark("");
+			dto.setReveiverAddress("甘肃省天水市甘谷县金山镇蒲家山村王瑶西小组");
+			dto.setReveiverArea("19267");
+			dto.setReveiverAreaName("甘谷县");
+			dto.setReveiverCity("44342");
+			dto.setReveiverCityName("天水市");
+			dto.setReveiverName("王占生");
+			dto.setReveiverProvince("24");
+			dto.setReveiverProvinceName("甘肃省");
+			dto.setReveiverTelphoneNo("0150****4855");
+
+			System.err.println(this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(dto));
+//			System.err.println(this.orderOprService.checkOrder(dto));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
