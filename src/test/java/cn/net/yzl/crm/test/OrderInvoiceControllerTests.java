ecfaf21d4ae58b62e3adaadbff61942f0b4ebc62
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-03-09 04:14:50 +0800

    OrderInvoiceControllerTests.java

--- src/test/java/cn/net/yzl/crm/test/OrderInvoiceControllerTests.java
+++ src/test/java/cn/net/yzl/crm/test/OrderInvoiceControllerTests.java
@@ -5,6 +5,8 @@ import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import cn.net.yzl.activity.model.requestModel.AccountRequest;
 import cn.net.yzl.crm.controller.order.OrderInvoiceController;
 
@@ -18,6 +20,8 @@ import cn.net.yzl.crm.controller.order.OrderInvoiceController;
 public class OrderInvoiceControllerTests {
 	@Resource
 	private OrderInvoiceController controller;
+	@Resource
+	private ObjectMapper objectMapper;
 
 	@Test
 	public void testGetMemberCoupon() {
@@ -37,8 +41,9 @@ public class OrderInvoiceControllerTests {
 			AccountRequest request = new AccountRequest();
 			request.setPageNo(1);
 			request.setPageSize(10);
-			request.setMemberCard("常立雷");
-			System.err.println(this.controller.getMemberIntegralRecords(request).getData().getItems());
+//			request.setMemberCard("常立雷");
+			System.err.println(this.objectMapper.writerWithDefaultPrettyPrinter()
+					.writeValueAsString(this.controller.getMemberIntegralRecords(request)));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
