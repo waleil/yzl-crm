b7dab6d887ff655b8afe0fd4eb2885a2fee1034e
李贺彦 <w-liheyan@yuzhilin.net.cn>
2021-03-08 09:14:36 +0800

    审核拨打次数调整

--- src/main/java/cn/net/yzl/crm/controller/workorder/CtiApiController.java
+++ src/main/java/cn/net/yzl/crm/controller/workorder/CtiApiController.java
@@ -8,12 +8,15 @@ import cn.net.yzl.workorder.model.dto.CticheckDTO;
 import cn.net.yzl.workorder.model.vo.CallInfoVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -45,8 +48,8 @@ public class CtiApiController {
 
     @GetMapping("/v1/checkCallCount")
     @ApiOperation(value = "审核拨打次数")
-    public ComResponse checkCallCount(){
-        return ctiApiClient.checkCallCount(QueryIds.userNo.get());
+    public ComResponse checkCallCount(@NotNull(message = "工单类型校验失败") @ApiParam("工单类型 1:热线工单，2回访工单") @RequestParam("workOrderType") Integer workOrderType){
+        return ctiApiClient.checkCallCount(QueryIds.userNo.get(),workOrderType);
     }
 
 }
