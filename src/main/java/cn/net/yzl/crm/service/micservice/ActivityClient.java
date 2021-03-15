bf79ad414e2c422f5df6300f0c1782215beb83b9
zhangweiwei <zhangweiwei@yuzhilin.net.cn>
2021-03-15 16:50:27 +0800

    ActivityClient.java

--- src/main/java/cn/net/yzl/crm/service/micservice/ActivityClient.java
+++ src/main/java/cn/net/yzl/crm/service/micservice/ActivityClient.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import cn.net.yzl.activity.model.requestModel.AccountHistoryRequest;
 import cn.net.yzl.activity.model.requestModel.AccountRequest;
 import cn.net.yzl.activity.model.requestModel.AccountWithOutPageRequest;
+import cn.net.yzl.activity.model.requestModel.CalculateOrderRequest;
 import cn.net.yzl.activity.model.requestModel.CalculateRequest;
 import cn.net.yzl.activity.model.requestModel.CheckOrderAmountRequest;
 import cn.net.yzl.activity.model.requestModel.ListAccountRequest;
@@ -22,6 +23,7 @@ import cn.net.yzl.activity.model.requestModel.OrderSubmitRequest;
 import cn.net.yzl.activity.model.requestModel.ProductDiscountRequest;
 import cn.net.yzl.activity.model.requestModel.ProductListDiscountRequest;
 import cn.net.yzl.activity.model.requestModel.RejectionOrderRequest;
+import cn.net.yzl.activity.model.responseModel.CalculationOrderResponse;
 import cn.net.yzl.activity.model.responseModel.MemberAccountHistoryResponse;
 import cn.net.yzl.activity.model.responseModel.MemberAccountResponse;
 import cn.net.yzl.activity.model.responseModel.MemberCouponResponse;
@@ -131,6 +133,17 @@ public interface ActivityClient {
 	@PostMapping("db/v1/calculate")
 	ComResponse<ProductPriceResponse> calculate(@RequestBody CalculateRequest request);
 
+	/**
+	 * 购物车计算金额
+	 * 
+	 * @param request {@link CalculateOrderRequest}
+	 * @return {@link CalculationOrderResponse}
+	 * @author zhangweiwei
+	 * @date 2021年3月15日,下午4:48:46
+	 */
+	@PostMapping("db/v1/calculateOrder")
+	ComResponse<CalculationOrderResponse> calculateOrder(@RequestBody CalculateOrderRequest request);
+
 	@ApiOperation(value = "校验订单金额")
 	@PostMapping("db/v1/checkOrderAmount")
 	ComResponse<List<ProductPriceResponse>> checkOrderAmount(@RequestBody CheckOrderAmountRequest request);
@@ -188,10 +201,11 @@ public interface ActivityClient {
 	 */
 	@PostMapping("/db/v1/orderSubmit")
 	ComResponse<OrderSubmitResponse> orderSubmit(@RequestBody OrderSubmitRequest request);
+
 	/**
 	 * 提交订单送积分和优惠券
 	 *
-	 * @param  memberCards
+	 * @param memberCards
 	 * @return {@link OrderSubmitResponse}
 	 * @author zhangweiwei
 	 * @date 2021年2月21日,上午5:08:19
