327faaaec62d7bba6d75def89b1ffe0631d7df76
马金宝 <w-majinbao@yuzhilin.net.cn>
2021-03-17 11:18:51 +0800

    fastDFSURL

--- src/main/java/cn/net/yzl/crm/service/product/impl/ProductServiceImpl.java
+++ src/main/java/cn/net/yzl/crm/service/product/impl/ProductServiceImpl.java
@@ -5,11 +5,15 @@ import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.crm.client.product.ProductClient;
 import cn.net.yzl.crm.config.FastDFSConfig;
 import cn.net.yzl.crm.service.product.ProductService;
+import cn.net.yzl.product.model.db.ProductAtlasBean;
 import cn.net.yzl.product.model.vo.product.dto.*;
 import cn.net.yzl.product.model.vo.product.vo.*;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -71,7 +75,27 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ComResponse<List<ProductAtlasDTO>> queryProductListAtlasByDiseaseName(String diseaseName) {
-        return productClient.queryProductListAtlasByDiseaseName(diseaseName);
+        ComResponse<List<ProductAtlasDTO>> listComResponse = productClient.queryProductListAtlasByDiseaseName(diseaseName);
+        if(!CollectionUtils.isEmpty(listComResponse.getData())) {
+            List<ProductAtlasDTO> data = listComResponse.getData();
+            data.stream().forEach(d->{
+                if (!CollectionUtils.isEmpty(d.getProductAtlasResult())) {
+                    List<Map<Integer, List<ProductAtlasBean>>> productAtlasResult = d.getProductAtlasResult();
+                    productAtlasResult.stream().forEach(map->{
+                        if(!map.isEmpty()){
+                            map.forEach((k,v)->{
+                                if (!CollectionUtils.isEmpty(v)){
+                                    v.stream().forEach(bean ->{
+                                        bean.setFastDFSUrl(StringUtils.isNotBlank(bean.getImageUrl())?fastDFSConfig.getUrl():null);
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+        return listComResponse;
     }
 
     @Override
