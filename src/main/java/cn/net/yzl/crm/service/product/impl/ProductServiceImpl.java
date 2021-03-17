a05d778e1e8ae8af6ba148a73a9174a07c3e7107
马金宝 <w-majinbao@yuzhilin.net.cn>
2021-03-17 15:33:03 +0800

    fastDFSURL

--- src/main/java/cn/net/yzl/crm/service/product/impl/ProductServiceImpl.java
+++ src/main/java/cn/net/yzl/crm/service/product/impl/ProductServiceImpl.java
@@ -5,15 +5,12 @@ import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.crm.client.product.ProductClient;
 import cn.net.yzl.crm.config.FastDFSConfig;
 import cn.net.yzl.crm.service.product.ProductService;
-import cn.net.yzl.product.model.db.ProductAtlasBean;
 import cn.net.yzl.product.model.vo.product.dto.*;
 import cn.net.yzl.product.model.vo.product.vo.*;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class ProductServiceImpl implements ProductService {
